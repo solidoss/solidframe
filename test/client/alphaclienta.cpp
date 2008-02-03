0afb51ac2c3f70e502ecba8231c4581d1404fc0b
vipalade <vipalade@gmail.com>
2008-02-03 08:42:05 +0000

    added a test client for alpha protocol

---
+++ test/client/alphaclienta.cpp
@@ -0,0 +1,412 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <deque>
+
+#include "system/debug.h"
+#include "system/socketaddress.h"
+#include "system/thread.h"
+//#include "common/utils.h"
+#include "writer.h"
+
+using namespace std;
+//typedef unsigned long long uint64;
+enum {BSIZE = 1024};
+
+class Info{
+public:
+	void update(unsigned _pos, ulong _v);
+	unsigned pushBack();
+	void print();
+private:
+	vector<ulong>   v;
+	Mutex           m;
+};
+
+unsigned Info::pushBack(){
+	v.push_back(0);
+	return v.size() - 1;
+}
+
+void Info::update(unsigned _pos, ulong _v){
+	//m.lock();
+	v[_pos] = _v;
+	//m.unlock();
+}
+
+void Info::print(){
+	uint64 tot = 0;
+	//m.lock();
+	for(vector<ulong>::const_iterator it(v.begin()); it != v.end(); ++it){
+		/*cout<<*it<<' ';*/ tot += *it;
+	}
+	tot /= 1024;
+	cout<<tot<<'k'<<' '<<'\r'<<flush;
+	//m.unlock();
+}
+
+static Info inf;
+
+class AlphaThread: public Thread{
+public:
+	AlphaThread(
+		const char *_node, 
+		const char* _svice, 
+		const char *_path,
+		unsigned _pos,
+		int _cnt = ((unsigned)(0xfffffff)),
+		int _sleep = 1):wr(-1),sd(-1),ai(_node, _svice), pos(_pos), cnt(_cnt),slp(_sleep),path(_path){}
+	void run();
+private:
+	enum {BufLen = 2*1024};
+	int list(char *_pb);
+	int fetch(unsigned _idx, char *_pb);
+	typedef std::deque<string> StrDqTp;
+	AddrInfo    ai;
+	Writer      wr;
+	int         sd;
+	int         cnt;
+	int         slp;
+	const char  *path;
+	StrDqTp     sdq;
+	unsigned    pos;
+	ulong       readc;
+};
+
+void AlphaThread::run(){
+	if(ai.empty()){
+		idbg("No such address");
+		return;
+	}
+	AddrInfoIterator it(ai.begin());
+	sd = socket(it.family(), it.type(), it.protocol());
+	if(sd < 0){
+		idbg("error creating socket");
+		cout<<"failed socket "<<pos<<endl;
+		return;
+	}
+	//cout<<"before connect "<<pos<<endl;
+	if(connect(sd, it.addr(), it.size())){
+		idbg("failed connect");
+		cout<<"failed connect "<<pos<<endl;
+		return;
+	}
+	readc = 0;
+	wr.reinit(sd);
+	char buf[BufLen];
+	int rv = list(buf);
+	idbg("return value "<<rv);
+	inf.update(pos, readc);
+	cout<<"connected "<<pos<<endl;
+// 	for(StrDqTp::const_iterator it(sdq.begin()); it != sdq.end(); ++it){
+// 		cout<<'['<<*it<<']'<<endl;
+// 	}
+	if(rv) return;
+	ulong ul = pos;
+	ulong m = sdq.size() - 1;
+	while(!(rv = fetch(ul % m, buf))){
+		++ul;inf.update(pos, readc);//Thread::sleep(100);
+	}
+	idbg("return value "<<rv);
+}
+
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+template <class T, T C1>
+inline T* find(T *_pc){
+	while(*_pc != C1) ++_pc;
+	return _pc;
+}
+
+template <class T, T C1, T C2>
+inline T* find(T *_pc){
+	while(*_pc != C2 && *_pc != C1) ++_pc;
+	return _pc;
+}
+
+template <class T, T C1, T C2, T C3>
+inline T* find(T *_pc){
+	while(*_pc != C2 && *_pc != C1 && *_pc != C3) ++_pc;
+	return _pc;
+}
+template <class T, T C1, T C2, T C3>
+inline T* findNot(T *_pc){
+	while(*_pc == C2 || *_pc == C1 || *_pc == C3) ++_pc;
+	return _pc;
+}
+
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+
+int AlphaThread::list(char *_pb){
+	wr<<"s1 list "<<path<<crlf;
+	if(wr.flush()) return -1;
+	enum {
+		StartLine,
+		FirstSpace,
+		SecondSpace,
+		FindPathStart,
+		ReadPath,
+		ReadLF,
+		SkipLine,
+		SkipLineLF,
+		ReadFinalCR,
+		ReadFinalLF
+	};
+	int rc;
+	int state = StartLine;
+	const char *bpos;
+	const char *bend;
+	const char *bp;
+	while((rc = read(sd, _pb, BufLen - 1)) > 0){
+		bool b = true;
+		readc += rc;
+		bpos = _pb;
+		bend = bpos + rc;
+		_pb[rc] = '\0';
+		while(b){
+			switch(state){
+				case StartLine:
+					if(!*bpos){b = false; break;}
+					if(*bpos != '*'){state = SkipLine; break;}
+					++bpos;
+					state = FirstSpace;
+				case FirstSpace:
+					if(!*bpos){b = false; break;}
+					if(*bpos != ' '){state = SkipLine; break;}
+					++bpos;
+					state = SecondSpace;
+				case SecondSpace://after the size
+					bp = find<const char,' ','\0'>(bpos);
+					if(*bp){
+						bpos = bp + 1;
+						state = FindPathStart;
+					}else{
+						b = false; break;
+					}
+				case FindPathStart:
+					bp = find<const char,' ','\0'>(bpos);
+					if(*bp){
+						bpos = bp + 1;
+						sdq.push_back("");
+						state = ReadPath;
+					}else{
+						b = false; break;
+					}
+				case ReadPath:
+					bp = find<const char,'\r','\0'>(bpos);
+					sdq.back().append(bpos, bp - bpos);
+					if(*bp){
+						bpos = bp + 1;
+						state = ReadLF;
+					}else{
+						b = false; break;
+					}
+				case ReadLF:
+					if(!*bpos){b = false; break;}
+					if(*bpos != '\n'){return -2;}
+					++bpos;
+					state = StartLine;
+					break;
+				case SkipLine:
+					bp = find<const char, '\r', '@', '\0'>(bpos);
+					if(!*bp){b = false; break;}
+					bpos = bp + 1;
+					if(*bp == '\r'){
+						state = SkipLineLF;
+						break;
+					}
+					if(*bp == '@'){
+						state = ReadFinalCR;
+						break;
+					}
+					break;
+				case SkipLineLF:
+					if(!*bpos){b = false; break;}
+					if(*bpos != '\n'){return -3;}
+					++bpos;
+					state = StartLine;
+					break;
+				case ReadFinalCR:
+					if(!*bpos){b = false; break;}
+					if(*bpos != '\r'){return -4;}
+					++bpos;
+					state = ReadFinalLF;
+				case ReadFinalLF:
+					if(!*bpos){b = false; break;}
+					if(*bpos != '\n'){return -5;}
+					++bpos;
+					return 0;
+			}
+		}
+	}
+	return -1;
+}
+
+int AlphaThread::fetch(unsigned _idx, char *_pb){
+	wr<<"s2 fetch "<<sdq[_idx]<<crlf;
+	//cout<<_idx<<" "<<sdq[_idx]<<endl;
+	if(wr.flush()) return -1;
+	enum{
+		StartLine, FirstSpace, SecondSpace, LiteralStart,LiteralNumber,
+		ReadCR, ReadLF, ReadLit, ReadLitFinalCR, ReadLitFinalLF,
+		SkipLine, SkipLineLF, ReadFinalCR, ReadFinalLF
+	};
+	int rc;
+	int state = StartLine;
+	const char *bpos;
+	const char *bend;
+	const char *bp;
+	string lit;
+	ulong litlen;
+	while((rc = read(sd, _pb, BufLen - 1)) > 0){
+		readc += rc;
+		bool b = true;
+		bpos = _pb;
+		bend = bpos + rc;
+		_pb[rc] = '\0';
+		//cout<<'[';cout.write(_pb, rc);cout<<']'<<endl;
+		while(b){
+			switch(state){
+				case StartLine:
+					if(!*bpos){b = false; break;}
+					if(*bpos != '*'){state = SkipLine; break;}
+					++bpos;
+					state = FirstSpace;
+				case FirstSpace:
+					if(!*bpos){b = false; break;}
+					if(*bpos != ' '){state = SkipLine; break;}
+					++bpos;
+					state = SecondSpace;
+				case SecondSpace://after the size
+					bp = find<const char,' ','\0'>(bpos);
+					if(*bp){
+						bpos = bp + 1;
+						sdq.push_back("");
+						state = LiteralStart;
+					}else{
+						b = false; break;
+					}
+				case LiteralStart:
+					if(!*bpos){b = false; break;}
+					if(*bpos != '{'){state = SkipLine; break;}
+					++bpos;
+					lit.clear();
+					state = LiteralNumber;
+				case LiteralNumber:
+					bp = find<const char,'}','\0'>(bpos);
+					lit.append(bpos, bp - bpos);
+					if(*bp){
+						bpos = bp + 1;
+						state = ReadCR;
+						litlen = atoi(lit.c_str());
+					}else{
+						b = false; break;
+					}
+				case ReadCR:
+					if(!*bpos){b = false; break;}
+					if(*bpos != '\r') return -2;
+					++bpos;
+					state = ReadLF;
+				case ReadLF:
+					if(!*bpos){b = false; break;}
+					if(*bpos != '\n') return -3;
+					++bpos;
+					state = ReadLit;
+				case ReadLit:
+					if(litlen){
+						if(bpos == bend){b = false; break;}
+						int toread = bend - bpos;
+						if(toread > litlen) toread = litlen;
+						//cout.write(bpos, toread);
+						bpos += toread;
+						litlen -= toread;
+						break;
+					}else{
+						state = ReadFinalCR;
+					}
+				case ReadLitFinalCR:
+					if(!*bpos){b = false; break;}
+					if(*bpos != '\r'){assert(false); return -4;}
+					++bpos;
+					state = ReadFinalLF;
+				case ReadLitFinalLF:
+					if(!*bpos){b = false; break;}
+					if(*bpos != '\n') return -5;
+					++bpos;
+					state = StartLine;
+					break;
+				case SkipLine:
+					bp = find<const char, '\r', '@', '\0'>(bpos);
+					if(!*bp){b = false; break;}
+					if(*bp == '\r'){
+						bpos = bp + 1;
+						state = SkipLineLF;
+						break;
+					}
+					if(*bp == '@'){
+						state = ReadFinalCR;
+						bpos = bp + 1;
+						break;
+					}
+					break;
+				case SkipLineLF:
+					if(!*bpos){b = false; break;}
+					if(*bpos != '\n'){return -6;}
+					++bpos;
+					state = StartLine;
+					break;
+				case ReadFinalCR:
+					if(!*bpos){b = false; break;}
+					if(*bpos != '\r'){return -7;}
+					++bpos;
+					state = ReadFinalLF;
+				case ReadFinalLF:
+					if(!*bpos){b = false; break;}
+					if(*bpos != '\n'){return -8;}
+					++bpos;
+					//state = ReadFinalLF;
+					return 0;
+			}
+		}
+	}
+	return -1;
+}
+
+int main(int argc, char *argv[]){
+	if(argc != 5){
+		cout<<"Usage: alphaclient thcnt addr port path"<<endl;
+		return 0;
+	}
+	signal(SIGPIPE, SIG_IGN);
+	Thread::init();
+#ifdef UDEBUG
+	{
+	string s = "dbg/";
+	s+= argv[0]+2;
+	initDebug(s.c_str());
+	}
+#endif
+	int cnt = atoi(argv[1]);
+	for(int i = 0; i < cnt; ++i){
+		AlphaThread *pt = new AlphaThread(argv[2], argv[3], argv[4],inf.pushBack());
+		pt->start(false, true, 24*1024);
+	}
+	while(true){
+		inf.print();
+		Thread::sleep(200);
+	}
+	Thread::waitAll();
+	cout<<"done"<<endl;
+	return 0;
+}
+
+
---
+++ test/client/writer.h
@@ -0,0 +1,190 @@
+#ifndef IMAPWRITER_H
+#define IMAPWRITER_H
+
+//#include "../utils.h"
+#include <string>
+#include "system/common.h"
+#include <list>
+
+#define APPSTR(str) str, sizeof(str) - 1
+
+typedef std::string String;
+
+
+class Writer{
+public://nonstatic methods
+    enum{BUFFLEN = 1024};
+    static bool isquotedspecial(uint8 _c);
+    Writer(int _sd):sd(_sd),wrerr(0),bend(bbeg+BUFFLEN),bpos(bbeg),count(0){}
+    ~Writer(){}
+    void reinit(int _sd);
+    int flush();
+    bool isOk()const{
+        return (wrerr >= 0);
+    }
+    void put(char _c){
+        if(bpos != bend){ *(bpos++)=_c; return;}
+        flush();
+        *(bpos++)=_c;
+    }
+    void put(char _c1, char _c2){
+        if(bpos < (bend -1)) {*(bpos++) = _c1;*(bpos++) = _c2; return;}
+        flush();
+        *(bpos++) = _c1;*(bpos++) = _c2;
+    }
+    void put(const char *_s,uint32 _sz);
+    void put(uint32 _v);
+    void put(const char* _str){
+        put(_str,strlen(_str));
+    }
+    uint32 getWriteCount(bool){
+        uint32  rc = count;
+        count = 0;
+        return rc;
+    }
+    uint32 getWriteCount()const{
+        return count;
+    }
+    Writer& operator << (char _c){ put(_c); return *this;}
+    Writer& operator << (const char* _s){
+        put(_s,strlen(_s)); return *this;
+    }
+    Writer& operator << (const String &_str){
+        put(_str.c_str(),_str.size()); return *this;
+    }
+    Writer& operator << (uint32 _v){
+        put(_v); return *this;
+    }
+    Writer& operator << ( Writer& (*f)(Writer&)){ return f(*this);}
+    struct atomstring{
+        const char* str;
+        ushort      len;
+    };
+    Writer& operator<<(atomstring _qs){
+        put(_qs.str,_qs.len); return *this;
+    }
+    struct qstring{
+        const char* str;
+        ushort      len;
+    };
+    Writer& operator<<(qstring _qs){
+        put('\"');put(_qs.str,_qs.len);put('\"'); return *this;
+    }
+    struct lstring{
+        const char* str;
+        ushort      len;
+    };
+    Writer& operator<<(lstring _qs){
+        put('{');
+        put((uint32) _qs.len);
+        put(APPSTR("}\r\n"));
+        put(_qs.str,_qs.len);
+        return *this;
+    }
+    struct astring{
+        const char* str;
+        ushort      len;
+    };
+    Writer& operator<<(astring _qs){
+        const char *st=_qs.str;
+        if(_qs.len > 64){
+            //st +=_qs.len;
+        }else{
+            while(*st!='\0'){
+                uint8 c=*st;
+                if(isquotedspecial(c)) break;
+                ++st;
+            }
+        }
+        if(*st!='\0'){
+            //we send a literal
+            put('{');
+            put((uint32) _qs.len);
+            put(APPSTR("}\r\n"));
+            put(_qs.str,_qs.len);
+            return *this;
+        }
+        put('\"');put(_qs.str,_qs.len);put('\"');
+        return *this;
+    }
+    struct littp{
+        uint32       litlen;
+        //Stream      *pstream;
+    };
+    Writer& operator<<(littp _t){
+        put('{');
+        put((uint32) _t.litlen);
+        put(APPSTR("}\r\n"));
+        flush();
+        if(_t.litlen == 0) return *this;
+        //INT64 len=_t.litlen;
+        //Stream::copy(os,*_t.pstream,len);
+        return *this;
+    }
+private:
+    int	        	sd;
+    int             wrerr;
+    char            bbeg[BUFFLEN+4];
+    char            *bend;
+    char            *bpos;
+    uint32           count;
+};
+inline Writer & flush(Writer & _rout){
+    _rout.flush(); return _rout;
+}
+
+inline Writer & crlf(Writer & _rout){
+    _rout.put('\r','\n'); return _rout;
+}
+///atom
+inline Writer::atomstring atom(const char* _str, ushort _len){
+    Writer::atomstring qs; qs.str= _str; qs.len = _len;
+    return qs;
+}
+inline Writer::atomstring atom(const String &_str){
+    Writer::atomstring qs; qs.str= _str.data(); qs.len = _str.size();
+    return qs;
+}
+///quoted string
+inline Writer::qstring qstr(const char* _str){
+    Writer::qstring qs; qs.str= _str; qs.len = strlen(_str);
+    return qs;
+}
+inline Writer::qstring qstr(const char* _str, ushort _len){
+    Writer::qstring qs; qs.str= _str; qs.len = _len;
+    return qs;
+}
+inline Writer::qstring qstr(const String &_str){
+    Writer::qstring qs; qs.str= _str.data(); qs.len = _str.size();
+    return qs;
+}
+///literal string
+inline Writer::lstring lstr(const char* _str){
+    Writer::lstring qs; qs.str= _str; qs.len = strlen(_str);
+    return qs;
+}
+inline Writer::lstring lstr(const char* _str, ushort _len){
+    Writer::lstring qs; qs.str= _str; qs.len = _len;
+    return qs;
+}
+inline Writer::lstring lstr(const String &_str){
+    Writer::lstring qs; qs.str= _str.data(); qs.len = _str.size();
+    return qs;
+}
+
+///astring
+inline Writer::astring astr(const char* _str){
+    Writer::astring qs; qs.str= _str; qs.len = strlen(_str);
+    return qs;
+}
+inline Writer::astring astr(const char* _str, ushort _len){
+    Writer::astring qs; qs.str= _str; qs.len = _len;
+    return qs;
+}
+inline Writer::astring astr(const String &_str){
+    Writer::astring qs; qs.str= _str.data(); qs.len = _str.size();
+    return qs;
+}
+
+#endif
+
