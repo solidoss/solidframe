309ef1ed1d3e6fde8245b6bfd3cd9d63ab20dbe3
vipalade <vipalade@gmail.com>
2008-12-15 11:35:17 +0000

    working ssl test server and client

--- test/extern/openssl/sslclient.cpp
+++ test/extern/openssl/sslclient.cpp
@@ -4,6 +4,7 @@
 #include "system/socketaddress.hpp"
 #include "system/socketdevice.hpp"
 #include "system/cassert.hpp"
+#include "system/thread.hpp"
 #include <cerrno>
 #include <cstring>
 #include <iostream>
@@ -13,6 +14,9 @@
 #include <unistd.h>
 
 using namespace std;
+
+#define CHK_SSL(err) if ((err)==-1) { ERR_print_errors_fp(stderr); exit(2); }
+
 
 int tocrlf(char *_pb, const char *_bend);
 
@@ -75,15 +79,9 @@ int main(int argc, char *argv[]){
 		cout<<"failed SSL_CTX_load_verify_locations 2 "<<ERR_error_string(ERR_get_error(), NULL)<<endl;;
 		return 0;
 	}
-	//BIO *bio = BIO_new_ssl_connect(ctx);//BIO_new_socket(sd.descriptor(), 0);
-	BIO *bio = BIO_new_socket(sd.descriptor(), BIO_NOCLOSE);
-	
-	//BIO_set_conn_hostname(bio, "localhost:4433");
-	
+	int err;
 	ssl = SSL_new(ctx);
-	//BIO_get_ssl(bio, &ssl);
-	SSL_set_mode(ssl, SSL_MODE_AUTO_RETRY);
-	
+
 	if(!ssl){
 		cout<<"failed SSL_new "<<ERR_error_string(ERR_get_error(), NULL)<<endl;;
 		return 0;
@@ -94,37 +92,44 @@ int main(int argc, char *argv[]){
 		return 0;
 	}
 	
-	SSL_set_bio(ssl, bio, bio);
-	BIO_set_ssl(bio, ssl, 0);
-	
-	if(SSL_connect(ssl) < 0){
-		fprintf(stderr, "Error connecting to server\n");
-		ERR_print_errors_fp(stderr);
-		return 0;
-	}
-	
-// 	if(BIO_do_connect(bio) <= 0) {
-// 		fprintf(stderr, "Error connecting to server\n");
-// 		ERR_print_errors_fp(stderr);
-// 		return 0;
-// 	}
-	cout<<"done connect"<<endl;
-// 	if(BIO_do_handshake(bio) <= 0) {
-// 		fprintf(stderr, "Error establishing SSL connection\n");
-// 		ERR_print_errors_fp(stderr);
-// 		return 0;
-// 	}
+	sd.makeNonBlocking();
+	err = SSL_set_fd(ssl, sd.descriptor());
+	cout<<"done ssl_set_fd "<<err<<endl;
+	ERR_print_errors_fp(stderr);
+	//CHK_SSL(err);
 
+	//SSL_set_mode(ssl, SSL_MODE_AUTO_RETRY);
+	while(true){
+		err = SSL_connect(ssl);
+		if(err < 0){
+			if(SSL_want(ssl) != SSL_NOTHING){
+				cout<<"want something"<<endl;
+				if(SSL_want_read(ssl)){
+					cout<<"want read"<<endl;
+				}
+				if(SSL_want_write(ssl)){
+					cout<<"want write"<<endl;
+				}
+				Thread::sleep(1000);
+			}else{
+				cout<<"error ssl_connect"<<endl;
+				return 10;
+			}
+		}else if(err == 0)
+			return 11;
+		else break;
+		
+	}
+	ERR_print_errors_fp(stderr);
+	cout<<"done SSL_connect"<<err<<endl;
+	CHK_SSL(err);
+	cout<<"done ssl connect"<<endl;
 	
 	cout<<"done ssl handshaking"<<endl;
-	sd.makeNonBlocking();
 	char	bufs[2][1024 + 128];
 	char	*wbuf[2];
 	int		readsz[2];
 	int		states[2];
-	//SSL_get_error
-	//SSL_ERROR_WANT_READ
-	//Edge triggered 
 	readsz[0] = 0;
 	bool read_socket = true, read_stdin = false;
 	bool write_socket = false;
@@ -132,8 +137,6 @@ int main(int argc, char *argv[]){
 	bool wait_write_on_read = false;
 	bool wait_write_on_write = false;
 	while(true){
-/*		int pollcnt = poll(pfds, 2, -1);
-		if(pollcnt <= 0) continue;*/
 		if(pfds[0].revents & POLLIN && !readsz[0]){
 			read_stdin = true;
 		}
@@ -156,10 +159,11 @@ int main(int argc, char *argv[]){
 			pfds[0].events = 0;//dont wait for stdin
 			write_socket = true;
 			read_stdin = false;
+			if(memcmp(bufs[0], "quit", 4) == 0) break;
 		}
 		
 		if(write_socket){//write to socket
-			int rv = BIO_write(bio, wbuf[0], readsz[0]);
+			int rv = SSL_write(ssl, wbuf[0], readsz[0]);
 			wait_write_on_write = false;
 			wait_read_on_write = false;
 			if(!wait_write_on_read){
@@ -169,14 +173,14 @@ int main(int argc, char *argv[]){
 				readsz[0] = 0;
 				write_socket = false;
 				pfds[0].events = POLLIN;//dont wait for stdin
-			}else if(BIO_should_retry(bio)){
+			}else if(SSL_want(ssl) != SSL_NOTHING){
 				cassert(pfds[1].events);
 				write_socket = false;
-				if(BIO_should_read(bio)){
+				if(SSL_want_read(ssl)){
 					pfds[1].events |= POLLIN;
 					wait_read_on_write = true;
 				}
-				if(BIO_should_write(bio)){
+				if(SSL_want_write(ssl)){
 					pfds[1].events |= POLLOUT;
 					wait_write_on_write = true;
 				}
@@ -191,7 +195,7 @@ int main(int argc, char *argv[]){
 		
 		if(read_socket){
 			//we can read:
-			int rv = BIO_read(bio, bufs[1], 8);
+			int rv = SSL_read(ssl, bufs[1], 8);
 			wait_write_on_read = false;
 			if(!wait_write_on_write){
 				pfds[1].events &= ~POLLOUT;
@@ -199,11 +203,11 @@ int main(int argc, char *argv[]){
 			if(rv > 0){
 				cout.write(bufs[1], rv).flush();
 				read_socket = true;
-			}else if(BIO_should_retry(bio)){
-				if(BIO_should_read(bio)){
+			}else if(SSL_want(ssl) != SSL_NOTHING){
+				if(SSL_want_read(ssl)){
 					pfds[1].events |= POLLIN;
 				}
-				if(BIO_should_write(bio)){
+				if(SSL_want_write(ssl)){
 					pfds[1].events |= POLLOUT;
 					wait_write_on_read = true;
 				}
@@ -215,6 +219,9 @@ int main(int argc, char *argv[]){
 		int pollcnt = poll(pfds, 2, (read_socket || read_stdin) ? 0 : -1);
 		if(pollcnt <= 0) continue;
 	}
+	
+	SSL_free(ssl);
+	cout<<sd.write("gigi", 4)<<endl;
 	return 0;
 }
 
@@ -244,54 +251,3 @@ int tocrlf(char *_pb, const char *_bend){
 	memcpy(_pb, b, len);
 	return len;
 }
-
-/*
-while(true){
-		int pollcnt = poll(pfds, 2, -1);
-		if(pollcnt <= 0) continue;
-		if(pfds[0].revents){//we can read from std in
-			readsz[0] = ::read(fileno(stdin), bufs[0], 1024);
-			wbuf[0] = bufs[0];
-			//write it on socket:
-			//int rv = sd.write(wbuf[0], readsz[0]);
-			int rv == readsz[0] = BIO_write(bio, wbuf[0], readsz[0]);
-			//if(rv < 0 && sd.shouldWait() || rv > 0 && rv < readsz[0]){
-			if(rv <= 0 && BIO_should_retry(bio)){
-				if(rv < 0) rv = 0;
-				readsz[0] -= rv;
-				wbuf[0] += rv;
-				pfds[1].events |= POLLOUT;//wait for ready to write
-				pfds[0].events = 0;//dont wait for input
-			}else if(rv > 0){//all was written
-				//do nothing
-				readsz[0] = 0;
-			}else{
-				break;
-			}
-		}
-		if(pfds[1].revents & POLLIN){//we can read from socket:
-			readsz[1] = sd.read(bufs[1], 1024);
-			if(readsz[1] < 0 && sd.shouldWait()){
-				//do nothing
-			}else if(readsz[1] > 0){//we've read something
-				cout.write(bufs[1], readsz[1]).flush();
-			}else{
-				break;
-			}
-		}
-		if(pfds[1].revents & POLLOUT){//we can write to socket:
-			int rv = sd.write(wbuf[0], readsz[0]);
-			if(rv < 0 && sd.shouldWait() || rv > 0 && rv < readsz[0]){
-				if(rv < 0) rv = 0;
-				readsz[0] -= rv;
-				wbuf[0] += rv;
-			}else if(rv > 0){//all was written
-				readsz[0] = 0;
-				wbuf[0] = NULL;
-				pfds[0].events |= POLLIN;//wait for ready to write
-			}else{
-				break;
-			}
-		}
-	}
-*/
