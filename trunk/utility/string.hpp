123ef222265ffec74a4aa90b91af1587a8efb7be
vipalade <vipalade@0b79f258-eccb-fe0a-0eba-1848281d3a48>
2009-04-23 10:30:58 +0000

    adding a dynamic linking test

--- trunk/utility/string.hpp
+++ trunk/utility/string.hpp
@@ -3,4 +3,16 @@
 
 const char * charToString(unsigned _c);
 
+//! Some cross platform cstring utility functions
+struct cstring{
+    //! Equivalent to strcmp
+    static int cmp(const char* _s1, const char *_s2);
+    //! Equivalent to strncmp
+    static int ncmp(const char* _s1, const char *_s2, uint _len);
+    //! Equivalent to strcasecmp
+    static int casecmp(const char* _s1, const char *_s2);
+    //! Equivalent to strncasecmp
+    static int ncasecmp(const char* _s1, const char *_s2, uint _len);
+};
+
 #endif
