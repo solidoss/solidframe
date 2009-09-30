13ed17f7fbc96782db14a5839771bc9c661465b8
vipalade <vipalade@0b79f258-eccb-fe0a-0eba-1848281d3a48>
2009-09-30 07:40:38 +0000

    added utility/functorstub.hpp changed functortestb.cpp

--- trunk/example/utility/functortest/functortestb.cpp
+++ trunk/example/utility/functortest/functortestb.cpp
@@ -1,189 +1,17 @@
 #include "system/debug.hpp"
-#include "system/common.hpp"
-
-struct NoType{};
-
-//================================================================================
-
-//================================================================================
-template <
-	unsigned DSZ,
-	class R,
-	class P1, class P2, class P3
->
-class FunctorBase;
-
-struct CallerBase{
-	virtual ~CallerBase(){}
-	virtual void destroy(void *_p){}
-	virtual size_t size()const{return 0;}
-};
-
-template <unsigned DSZ, class R>
-class FunctorBase<DSZ, R, NoType, NoType, NoType>{
-protected:
-	struct BaseCaller: CallerBase{
-		virtual R call(void *_p){return R();}
-	};
-	template <class T>
-	struct Caller: BaseCaller{
-		inline T& ref(void* _pv){
-			if(sizeof(T) <= DSZ)
-				return (*reinterpret_cast<T*>(_pv));
-			else
-				return (*reinterpret_cast<T*>(*reinterpret_cast<void**>(_pv)));
-		}
-		R call(void *_pv){
-			return ref(_pv)();
-		}
-		void destroy(void *_p){
-			if(sizeof(T) <= DSZ)
-				reinterpret_cast<T*>(_p)->~T();
-			else
-				reinterpret_cast<T*>(*reinterpret_cast<void**>(_p))->~T();
-		}
-		size_t size()const{return sizeof(T);}
-	};
-};
-
-template <unsigned DSZ, class R, class P1>
-class FunctorBase<DSZ, R, P1, NoType, NoType>{
-protected:
-	struct BaseCaller: CallerBase{
-		virtual R call(void *_p, P1){return R();}
-	};
-	template <class T>
-	struct Caller: BaseCaller{
-		inline T& ref(void* _pv){
-			if(sizeof(T) <= DSZ)
-				return (*reinterpret_cast<T*>(_pv));
-			else
-				return (*reinterpret_cast<T*>(*reinterpret_cast<void**>(_pv)));
-		}
-		R call(void *_pv, P1 _p1){
-			return ref(_pv)(_p1);
-		}
-		void destroy(void *_p){
-			if(sizeof(T) <= DSZ)
-				reinterpret_cast<T*>(_p)->~T();
-			else
-				reinterpret_cast<T*>(*reinterpret_cast<void**>(_p))->~T();
-		}
-		size_t size()const{return sizeof(T);}
-	};
-};
-
-template <unsigned DSZ, class R, class P1, class P2>
-class FunctorBase<DSZ, R, P1, P2, NoType>{
-protected:
-	struct BaseCaller: CallerBase{
-		virtual R call(void *_p, P1, P2){return R();}
-	};
-	template <class T>
-	struct Caller: BaseCaller{
-		inline T& ref(void* _pv){
-			if(sizeof(T) <= DSZ)
-				return (*reinterpret_cast<T*>(_pv));
-			else
-				return (*reinterpret_cast<T*>(*reinterpret_cast<void**>(_pv)));
-		}
-		R call(void *_pv, P1 _p1, P2 _p2){
-			return ref(_pv)(_p1, _p2);
-		}
-		void destroy(void *_p){
-			if(sizeof(T) <= DSZ)
-				reinterpret_cast<T*>(_p)->~T();
-			else
-				reinterpret_cast<T*>(*reinterpret_cast<void**>(_p))->~T();
-		}
-		size_t size()const{return sizeof(T);}
-	};
-};
-
-template <unsigned DSZ, class R, class P1, class P2, class P3>
-class FunctorBase{
-protected:
-	struct BaseCaller: CallerBase{
-		virtual R call(void *_p, P1, P2, P3){return R();}
-	};
-	template <class T>
-	struct Caller: BaseCaller{
-		inline T& ref(void* _pv){
-			if(sizeof(T) <= DSZ)
-				return (*reinterpret_cast<T*>(_pv));
-			else
-				return (*reinterpret_cast<T*>(*reinterpret_cast<void**>(_pv)));
-		}
-		R call(void *_pv, P1 _p1, P2 _p2, P3 _p3){
-			return ref(_pv)(_p1, _p2, _p3);
-		}
-		void destroy(void *_p){
-			if(sizeof(T) <= DSZ)
-				reinterpret_cast<T*>(_p)->~T();
-			else
-				reinterpret_cast<T*>(*reinterpret_cast<void**>(_p))->~T();
-		}
-		size_t size()const{return sizeof(T);}
-	};
-};
-
-template <
-	unsigned DSZ = sizeof(void*),
-	class R = void,
-	class P1 = NoType, class P2 = NoType, class P3 = NoType
->
-class FunctorStub;
-
-template <
-	unsigned DSZ,
-	typename R,
-	typename P1, typename P2, typename P3
->
-class FunctorStub: FunctorBase<
-	(DSZ >= sizeof(void*)) ? DSZ : sizeof(void*),
-	R, P1, P2, P3
->{
-	typedef FunctorBase<(DSZ >= sizeof(void*)) ? DSZ : sizeof(void*), R, P1, P2, P3> BaseTp;
-	typedef typename BaseTp::BaseCaller	BaseCallerTp;
-	enum {
-		StoreBufSize = (DSZ >= sizeof(void*)) ? DSZ : sizeof(void*)
-	};
-public:
-	FunctorStub(){
-		new(callerbuf) BaseCallerTp;
-	}
-	template <class T>
-	void operator=(const T &_rt){
-		reinterpret_cast<BaseCallerTp*>(callerbuf)->destroy(storebuf);
-		reinterpret_cast<BaseCallerTp*>(callerbuf)->~BaseCaller();
-		new(callerbuf) typename BaseTp::template Caller<T>;
-		if(sizeof(T) <= StoreBufSize){
-			new(storebuf) T(_rt);
-		}else{
-			*reinterpret_cast<void**>(storebuf) = new T(_rt);
-		}
-	}
-	R operator()(){
-		return reinterpret_cast<BaseCallerTp*>(callerbuf)->call(storebuf);
-	}
-	R operator()(P1 _p1){
-		return reinterpret_cast<BaseCallerTp*>(callerbuf)->call(storebuf, _p1);
-	}
-	R operator()(P1 _p1, P2 _p2){
-		return reinterpret_cast<BaseCallerTp*>(callerbuf)->call(storebuf, _p1, _p2);
-	}
-	R operator()(P1 _p1, P2 _p2, P3 _p3){
-		return reinterpret_cast<BaseCallerTp*>(callerbuf)->call(storebuf, _p1, _p2, _p3);
-	}
-private:
-	char callerbuf[sizeof(BaseCallerTp)];
-	char storebuf[StoreBufSize];
-};
-
-//================================================================================
+#include "utility/functorstub.hpp"
 
 struct TestAFunctor{
-	TestAFunctor(int _v):v(_v){}
+	TestAFunctor(int _v):v(_v){
+		idbg("");
+	}
+	TestAFunctor(const TestAFunctor &_rv){
+		idbg("");
+		*this = _rv;
+	}
+	~TestAFunctor(){
+		idbg("");
+	}
 	int operator()(){
 		idbg("v = "<<v);
 		return v;
@@ -250,10 +78,15 @@ struct TestFFunctor{
 	void operator()(const std::string &_rs, int _x, const char *_str){
 		idbg("v = "<<v<<" string = "<<_rs<<" x = "<<_x<<" str = "<<_str);
 	}
+	int operator()(const std::string &_rs, int _x, const char *_str, char _c, short _s, ushort _us, ulong _ul){
+		idbg("v = "<<v<<" rs = "<<_rs<<" x = "<<_x<<" _str = "<<_str<<" c = "<<_c<<" s = "<<_s<<" us = "<<_us<<" ul = "<<_ul);
+		return v;
+	}
 	int v;
 	int t;
 	int c;
 };
+
 
 int main(int argc, char *argv[]){
 	{
@@ -262,18 +95,20 @@ int main(int argc, char *argv[]){
 		Dbg::instance().moduleMask("all");
 	}
 	//FunctorStub<NoType>				fnc0;
-	FunctorStub<4, int>								fnca;
-	FunctorStub<4, void, const std::string&>		fncb;
-	FunctorStub<4, int, const std::string&>			fncc;
-	FunctorStub<4, int, int, const std::string&>	fncd;
-	FunctorStub<4, void, const std::string&, int>	fnce;
+	FunctorStub<3 * sizeof(int), int>							fnca;
+	FunctorStub<4, void, const std::string&>					fncb;
+	FunctorStub<4, int, const std::string&>						fncc;
+	FunctorStub<4, int, int, const std::string&>				fncd;
+	FunctorStub<4, void, const std::string&, int>				fnce;
 	FunctorStub<4, void, const std::string&, int, const char *>	fncf;
+	FunctorStub<3 * sizeof(int), int, const std::string&, int, const char*, char, short, ushort, ulong> fncf2;
 	fnca = TestAFunctor(11);
 	fncb = TestBFunctor(12);
 	fncc = TestCFunctor(13);
 	fncd = TestDFunctor(14);
 	fnce = TestEFunctor(15);
 	fncf = TestFFunctor(16);
+	fncf2 = TestFFunctor(17);
 	//fncc = TestAFunctor(14);
 	int v;
 	v = fnca();
@@ -285,6 +120,8 @@ int main(int argc, char *argv[]){
 	idbg("v = "<<v);
 	fnce("altceva2", 3);
 	fncf("altceva2", 3, "super");
+	v = fncf2("alt", 4, "mega", 'c', 1, 2, 3);
+	idbg("v = "<<v);
 	return 0;
 }
 
---
+++ trunk/utility/functorstub.hpp
@@ -0,0 +1,316 @@
+#ifndef UTILITY_FUNCTORSTUB_HPP
+#define UTILITY_FUNCTORSTUB_HPP
+
+#include "system/common.hpp"
+
+
+//--------------------------------------------------------------------------------------
+// 		FunctorBase
+//--------------------------------------------------------------------------------------
+template <
+	unsigned DSZ,
+	class R,
+	class P1, class P2, class P3, class P4, class P5, class P6, class P7
+>
+class FunctorBase;
+
+struct CallerBase{
+	virtual ~CallerBase(){}
+	virtual void destroy(void *_p){}
+	virtual size_t size()const{return 0;}
+};
+
+template <unsigned DSZ, class R>
+class FunctorBase<DSZ, R, EmptyType, EmptyType, EmptyType, EmptyType, EmptyType, EmptyType, EmptyType>{
+protected:
+	struct BaseCaller: CallerBase{
+		virtual R call(void *_p){return R();}
+	};
+	template <class T>
+	struct Caller: BaseCaller{
+		inline T& ref(void* _pv){
+			if(sizeof(T) <= DSZ)
+				return (*reinterpret_cast<T*>(_pv));
+			else
+				return (*reinterpret_cast<T*>(*reinterpret_cast<void**>(_pv)));
+		}
+		R call(void *_pv){
+			return ref(_pv)();
+		}
+		void destroy(void *_p){
+			if(sizeof(T) <= DSZ)
+				reinterpret_cast<T*>(_p)->~T();
+			else
+				reinterpret_cast<T*>(*reinterpret_cast<void**>(_p))->~T();
+		}
+		size_t size()const{return sizeof(T);}
+	};
+};
+
+template <unsigned DSZ, class R, class P1>
+class FunctorBase<DSZ, R, P1, EmptyType, EmptyType, EmptyType, EmptyType, EmptyType, EmptyType>{
+protected:
+	struct BaseCaller: CallerBase{
+		virtual R call(void *_p, P1){return R();}
+	};
+	template <class T>
+	struct Caller: BaseCaller{
+		inline T& ref(void* _pv){
+			if(sizeof(T) <= DSZ)
+				return (*reinterpret_cast<T*>(_pv));
+			else
+				return (*reinterpret_cast<T*>(*reinterpret_cast<void**>(_pv)));
+		}
+		R call(void *_pv, P1 _p1){
+			return ref(_pv)(_p1);
+		}
+		void destroy(void *_p){
+			if(sizeof(T) <= DSZ)
+				reinterpret_cast<T*>(_p)->~T();
+			else
+				reinterpret_cast<T*>(*reinterpret_cast<void**>(_p))->~T();
+		}
+		size_t size()const{return sizeof(T);}
+	};
+};
+
+template <unsigned DSZ, class R, class P1, class P2>
+class FunctorBase<DSZ, R, P1, P2, EmptyType, EmptyType, EmptyType, EmptyType, EmptyType>{
+protected:
+	struct BaseCaller: CallerBase{
+		virtual R call(void *_p, P1, P2){return R();}
+	};
+	template <class T>
+	struct Caller: BaseCaller{
+		inline T& ref(void* _pv){
+			if(sizeof(T) <= DSZ)
+				return (*reinterpret_cast<T*>(_pv));
+			else
+				return (*reinterpret_cast<T*>(*reinterpret_cast<void**>(_pv)));
+		}
+		R call(void *_pv, P1 _p1, P2 _p2){
+			return ref(_pv)(_p1, _p2);
+		}
+		void destroy(void *_p){
+			if(sizeof(T) <= DSZ)
+				reinterpret_cast<T*>(_p)->~T();
+			else
+				reinterpret_cast<T*>(*reinterpret_cast<void**>(_p))->~T();
+		}
+		size_t size()const{return sizeof(T);}
+	};
+};
+
+template <unsigned DSZ, class R, class P1, class P2, class P3>
+class FunctorBase<DSZ, R, P1, P2, P3, EmptyType, EmptyType, EmptyType, EmptyType>{
+protected:
+	struct BaseCaller: CallerBase{
+		virtual R call(void *_p, P1, P2, P3){return R();}
+	};
+	template <class T>
+	struct Caller: BaseCaller{
+		inline T& ref(void* _pv){
+			if(sizeof(T) <= DSZ)
+				return (*reinterpret_cast<T*>(_pv));
+			else
+				return (*reinterpret_cast<T*>(*reinterpret_cast<void**>(_pv)));
+		}
+		R call(void *_pv, P1 _p1, P2 _p2, P3 _p3){
+			return ref(_pv)(_p1, _p2, _p3);
+		}
+		void destroy(void *_p){
+			if(sizeof(T) <= DSZ)
+				reinterpret_cast<T*>(_p)->~T();
+			else
+				reinterpret_cast<T*>(*reinterpret_cast<void**>(_p))->~T();
+		}
+		size_t size()const{return sizeof(T);}
+	};
+};
+
+template <unsigned DSZ, class R, class P1, class P2, class P3, class P4>
+class FunctorBase<DSZ, R, P1, P2, P3, P4, EmptyType, EmptyType, EmptyType>{
+protected:
+	struct BaseCaller: CallerBase{
+		virtual R call(void *_p, P1, P2, P3, P4){return R();}
+	};
+	template <class T>
+	struct Caller: BaseCaller{
+		inline T& ref(void* _pv){
+			if(sizeof(T) <= DSZ)
+				return (*reinterpret_cast<T*>(_pv));
+			else
+				return (*reinterpret_cast<T*>(*reinterpret_cast<void**>(_pv)));
+		}
+		R call(void *_pv, P1 _p1, P2 _p2, P3 _p3, P4 _p4){
+			return ref(_pv)(_p1, _p2, _p3, _p4);
+		}
+		void destroy(void *_p){
+			if(sizeof(T) <= DSZ)
+				reinterpret_cast<T*>(_p)->~T();
+			else
+				reinterpret_cast<T*>(*reinterpret_cast<void**>(_p))->~T();
+		}
+		size_t size()const{return sizeof(T);}
+	};
+};
+template <unsigned DSZ, class R, class P1, class P2, class P3, class P4, class P5>
+class FunctorBase<DSZ, R, P1, P2, P3, P4, P5, EmptyType, EmptyType>{
+protected:
+	struct BaseCaller: CallerBase{
+		virtual R call(void *_p, P1, P2, P3, P4, P5){return R();}
+	};
+	template <class T>
+	struct Caller: BaseCaller{
+		inline T& ref(void* _pv){
+			if(sizeof(T) <= DSZ)
+				return (*reinterpret_cast<T*>(_pv));
+			else
+				return (*reinterpret_cast<T*>(*reinterpret_cast<void**>(_pv)));
+		}
+		R call(void *_pv, P1 _p1, P2 _p2, P3 _p3, P4 _p4, P5 _p5){
+			return ref(_pv)(_p1, _p2, _p3, _p4, _p5);
+		}
+		void destroy(void *_p){
+			if(sizeof(T) <= DSZ)
+				reinterpret_cast<T*>(_p)->~T();
+			else
+				reinterpret_cast<T*>(*reinterpret_cast<void**>(_p))->~T();
+		}
+		size_t size()const{return sizeof(T);}
+	};
+};
+template <unsigned DSZ, class R, class P1, class P2, class P3, class P4, class P5, class P6>
+class FunctorBase<DSZ, R, P1, P2, P3, P4, P5, P6, EmptyType>{
+protected:
+	struct BaseCaller: CallerBase{
+		virtual R call(void *_p, P1, P2, P3, P4, P5, P6){return R();}
+	};
+	template <class T>
+	struct Caller: BaseCaller{
+		inline T& ref(void* _pv){
+			if(sizeof(T) <= DSZ)
+				return (*reinterpret_cast<T*>(_pv));
+			else
+				return (*reinterpret_cast<T*>(*reinterpret_cast<void**>(_pv)));
+		}
+		R call(void *_pv, P1 _p1, P2 _p2, P3 _p3, P4 _p4, P5 _p5, P6 _p6){
+			return ref(_pv)(_p1, _p2, _p3, _p4, _p5, _p6);
+		}
+		void destroy(void *_p){
+			if(sizeof(T) <= DSZ)
+				reinterpret_cast<T*>(_p)->~T();
+			else
+				reinterpret_cast<T*>(*reinterpret_cast<void**>(_p))->~T();
+		}
+		size_t size()const{return sizeof(T);}
+	};
+};
+
+template <unsigned DSZ, class R, class P1, class P2, class P3, class P4, class P5, class P6, class P7>
+class FunctorBase{
+protected:
+	struct BaseCaller: CallerBase{
+		virtual R call(void *_p, P1, P2, P3, P4, P5, P6, P7){return R();}
+	};
+	template <class T>
+	struct Caller: BaseCaller{
+		inline T& ref(void* _pv){
+			if(sizeof(T) <= DSZ)
+				return (*reinterpret_cast<T*>(_pv));
+			else
+				return (*reinterpret_cast<T*>(*reinterpret_cast<void**>(_pv)));
+		}
+		R call(void *_pv, P1 _p1, P2 _p2, P3 _p3, P4 _p4, P5 _p5, P6 _p6, P7 _p7){
+			return ref(_pv)(_p1, _p2, _p3, _p4, _p5, _p6, _p7);
+		}
+		void destroy(void *_p){
+			if(sizeof(T) <= DSZ)
+				reinterpret_cast<T*>(_p)->~T();
+			else
+				reinterpret_cast<T*>(*reinterpret_cast<void**>(_p))->~T();
+		}
+		size_t size()const{return sizeof(T);}
+	};
+};
+
+//--------------------------------------------------------------------------------------
+//		FunctorStub
+//--------------------------------------------------------------------------------------
+
+template <
+	unsigned DSZ = sizeof(void*),
+	class R = void,
+	class P1 = EmptyType, class P2 = EmptyType, class P3 = EmptyType, class P4 = EmptyType, class P5 = EmptyType, class P6 = EmptyType, class P7 = EmptyType
+>
+class FunctorStub;
+
+template <
+	unsigned DSZ,
+	typename R,
+	typename P1, typename P2, typename P3, typename P4, typename P5, typename P6, typename P7
+>
+class FunctorStub: FunctorBase<
+	(DSZ >= sizeof(void*)) ? DSZ : sizeof(void*),
+	R, P1, P2, P3, P4, P5, P6, P7
+>{
+	typedef FunctorBase<(DSZ >= sizeof(void*)) ? DSZ : sizeof(void*), R, P1, P2, P3, P4, P5, P6, P7> BaseTp;
+	typedef typename BaseTp::BaseCaller	BaseCallerTp;
+	enum {
+		StoreBufSize = (DSZ >= sizeof(void*)) ? DSZ : sizeof(void*)
+	};
+public:
+	FunctorStub(){
+		new(callerbuf) BaseCallerTp;
+	}
+	~FunctorStub(){
+		reinterpret_cast<BaseCallerTp*>(callerbuf)->destroy(storebuf);
+		reinterpret_cast<BaseCallerTp*>(callerbuf)->~BaseCaller();
+	}
+	void clear(){
+		reinterpret_cast<BaseCallerTp*>(callerbuf)->destroy(storebuf);
+		reinterpret_cast<BaseCallerTp*>(callerbuf)->~BaseCaller();
+		new(callerbuf) BaseCallerTp;
+	}
+	template <class T>
+	void operator=(const T &_rt){
+		reinterpret_cast<BaseCallerTp*>(callerbuf)->destroy(storebuf);
+		reinterpret_cast<BaseCallerTp*>(callerbuf)->~BaseCaller();
+		new(callerbuf) typename BaseTp::template Caller<T>;
+		if(sizeof(T) <= StoreBufSize){
+			new(storebuf) T(_rt);
+		}else{
+			*reinterpret_cast<void**>(storebuf) = new T(_rt);
+		}
+	}
+	R operator()(){
+		return reinterpret_cast<BaseCallerTp*>(callerbuf)->call(storebuf);
+	}
+	R operator()(P1 _p1){
+		return reinterpret_cast<BaseCallerTp*>(callerbuf)->call(storebuf, _p1);
+	}
+	R operator()(P1 _p1, P2 _p2){
+		return reinterpret_cast<BaseCallerTp*>(callerbuf)->call(storebuf, _p1, _p2);
+	}
+	R operator()(P1 _p1, P2 _p2, P3 _p3){
+		return reinterpret_cast<BaseCallerTp*>(callerbuf)->call(storebuf, _p1, _p2, _p3);
+	}
+	R operator()(P1 _p1, P2 _p2, P3 _p3, P4 _p4){
+		return reinterpret_cast<BaseCallerTp*>(callerbuf)->call(storebuf, _p1, _p2, _p3, _p4);
+	}
+	R operator()(P1 _p1, P2 _p2, P3 _p3, P4 _p4, P5 _p5){
+		return reinterpret_cast<BaseCallerTp*>(callerbuf)->call(storebuf, _p1, _p2, _p3, _p4, _p5);
+	}
+	R operator()(P1 _p1, P2 _p2, P3 _p3, P4 _p4, P5 _p5, P6 _p6){
+		return reinterpret_cast<BaseCallerTp*>(callerbuf)->call(storebuf, _p1, _p2, _p3, _p4, _p5, _p6);
+	}
+	R operator()(P1 _p1, P2 _p2, P3 _p3, P4 _p4, P5 _p5, P6 _p6, P7 _p7){
+		return reinterpret_cast<BaseCallerTp*>(callerbuf)->call(storebuf, _p1, _p2, _p3, _p4, _p5, _p6, _p7);
+	}
+private:
+	char callerbuf[sizeof(BaseCallerTp)];
+	char storebuf[StoreBufSize];
+};
+
+
+#endif
