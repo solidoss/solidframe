23de64dbff94b0ac5e476a47c3d6e02736703424
vipalade <vipalade@0b79f258-eccb-fe0a-0eba-1848281d3a48>
2009-12-04 09:07:24 +0000

    added support to configure script for forward cmake parameters (-P)

--- trunk/check.config.cmake
+++ trunk/check.config.cmake
@@ -8,7 +8,8 @@ check_include_files(pthread.h HAVE_PTHREAD_H)
 # check if function local static variables are thread safe
 file (READ "${CMAKE_CURRENT_SOURCE_DIR}/check/safestatic.cpp" source_code)
 
-message("source code: ${source_code}")
+# message("source code: ${source_code}")
+set(CMAKE_REQUIRED_LIBRARIES ${SYS_BASIC_LIBS})
 
 CHECK_CXX_SOURCE_RUNS("${source_code}" IS_SAFE_FUNCTION_STATIC)
 
