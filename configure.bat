8b2c2ceefe0e5cee2944b0b2527f1ebe0dbaff07
vipalade <vipalade@gmail.com>
2010-03-19 11:27:33 +0000

    working on configure.ps1 for windows powershell

--- configure.bat
+++ configure.bat
@@ -1,5 +1,9 @@
-mkdir build
-cd build
-mkdir vstudio9
-cd vstudio9
-cmake -G "Visual Studio 9 2008" -D CMAKE_BUILD_TYPE=nolog ..\..\
+@echo off
+echo.
+echo Please use PowerShell script configure.ps1 instead.
+echo.
+echo You also must enable local scripts by:
+echo set-executionpolicy remotesigned
+echo.
+echo Trying to run configure.ps1 to see help:
+powershell .\configure.ps1
