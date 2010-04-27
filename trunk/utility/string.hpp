de73926d63ba1ffaea1f92c058afdff92e9a676b
vipalade <vipalade@0b79f258-eccb-fe0a-0eba-1848281d3a48>
2010-04-27 16:23:20 +0000

    fixed a bug in cstring wrapper

--- trunk/utility/string.hpp
+++ trunk/utility/string.hpp
@@ -1,6 +1,8 @@
 #ifndef UTILITY_STRING_HPP
 #define UTILITY_STRING_HPP
 
+#include "system/common.hpp"
+
 const char * charToString(unsigned _c);
 
 //! Some cross platform cstring utility functions
@@ -13,6 +15,28 @@ struct cstring{
     static int casecmp(const char* _s1, const char *_s2);
     //! Equivalent to strncasecmp
     static int ncasecmp(const char* _s1, const char *_s2, uint _len);
+    template <class T, T C1>
+	static T* find(T *_pc){
+		while(*_pc != C1) ++_pc;
+		return _pc;
+	}
+	
+	template <class T, T C1, T C2>
+	static T* find(T *_pc){
+		while(*_pc != C2 && *_pc != C1) ++_pc;
+		return _pc;
+	}
+	
+	template <class T, T C1, T C2, T C3>
+	static T* find(T *_pc){
+		while(*_pc != C2 && *_pc != C1 && *_pc != C3) ++_pc;
+		return _pc;
+	}
+	template <class T, T C1, T C2, T C3>
+	static T* findNot(T *_pc){
+		while(*_pc == C2 || *_pc == C1 || *_pc == C3) ++_pc;
+		return _pc;
+}
 };
 
 #endif
