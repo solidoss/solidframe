ab12f357eb135a9250a50d4e45c0532c04a7cba4
vipalade <vipalade@gmail.com>
2010-10-28 10:43:14 +0000

    rebranding step 2

--- example/system/thread/tests.cpp
+++ example/system/thread/tests.cpp
@@ -3,20 +3,20 @@
 	Copyright 2007, 2008 Valentin Palade
 	vipalade@gmail.com
 
-	This file is part of SolidGround framework.
+	This file is part of SolidFrame framework.
 
-	SolidGround is free software: you can redistribute it and/or modify
+	SolidFrame is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
-	SolidGround is distributed in the hope that it will be useful,
+	SolidFrame is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
 
 	You should have received a copy of the GNU General Public License
-	along with SolidGround.  If not, see <http://www.gnu.org/licenses/>.
+	along with SolidFrame.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <iostream>
@@ -147,7 +147,7 @@ void Runner::run(){
 }
 
 int main(int argc, char *argv[]){
-	cout<<"Built on SolidGround version "<<SG_MAJOR<<'.'<<SG_MINOR<<'.'<<SG_PATCH<<endl;
+	cout<<"Built on SolidFrame version "<<SF_MAJOR<<'.'<<SF_MINOR<<'.'<<SF_PATCH<<endl;
 	{
 	//initDebug(s.c_str());
 		Dbg::instance().initStdErr();
