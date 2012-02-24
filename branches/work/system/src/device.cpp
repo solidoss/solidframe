aa695a4d95af47f619da86855912efecaee60711
vipalade <vipalade@0b79f258-eccb-fe0a-0eba-1848281d3a48>
2012-02-24 13:48:58 +0000

    adding support for wsa cleanup on non debug build

--- branches/work/system/src/device.cpp
+++ branches/work/system/src/device.cpp
@@ -496,8 +496,22 @@ int do_erase_file(WCHAR *_pwc, const char *_path, size_t _sz, size_t _wcp){
 #endif
 }
 
+#ifndef UDEBUG
+#ifdef ON_WINDOWS
+struct wsa_cleaner{
+	~wsa_cleaner(){
+		WSACleanup();
+	}
+}
+#endif
+#endif
+
 //---- SocketDevice ---------------------------------
-SocketDevice::SocketDevice(const SocketDevice &_sd):Device(_sd){}
+SocketDevice::SocketDevice(const SocketDevice &_sd):Device(_sd){
+#ifndef UDEBUG
+	static const wsa_cleaner wsaclean;
+#endif
+}
 SocketDevice::SocketDevice(){
 }
 SocketDevice& SocketDevice::operator=(const SocketDevice &_dev){
