3597685add720de2d1dc1e749863c2802a07475a
vipalade <vipalade@0b79f258-eccb-fe0a-0eba-1848281d3a48>
2012-08-30 13:24:07 +0000

    remove work branch

--- branches/work/check/gnuatomic.cpp
+++
@@ -1,8 +0,0 @@
-#include <ext/atomicity.h>
-
-int main(int argc, char *argv[]){
-	int  r;
-	__gnu_cxx:: __atomic_add_dispatch(&r, 1);
-	__gnu_cxx::__exchange_and_add_dispatch(&r, -1);
-	return 0;
-}
