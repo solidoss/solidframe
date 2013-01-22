4cf75877e29df8217c6d2469c9ce6a76eb924bfc
vipalade <vipalade@0b79f258-eccb-fe0a-0eba-1848281d3a48>
2013-01-22 08:57:21 +0000

    no more no-asm in prepare_extern windows

--- branches/work/administration/windows/prepare_extern.bat
+++ branches/work/administration/windows/prepare_extern.bat
@@ -7,7 +7,7 @@ call bootstrap.bat
 call bjam.exe --without-mpi --without-python --prefix="%PWD%" --exec-prefix="%PWD%" link=static threading=multi variant=release --layout=system runtime-link=static install
 cd ..
 cd openssl-1.0.0g
-perl Configure VC-WIN32 no-asm --prefix="%PWD%"
+perl Configure VC-WIN32 --prefix="%PWD%"
 call ms\do_ms.bat
 nmake -f ms\nt.mak
 nmake -f ms\nt.mak install
