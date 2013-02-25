3d4f172fbf36fa17f0cde1d1045a91a40b4a8939
vipalade <vipalade@0b79f258-eccb-fe0a-0eba-1848281d3a48>
2013-02-25 14:13:08 +0000

    the manager interface

---
+++ branches/work/frame/manager.hpp
@@ -0,0 +1,119 @@
+/* Declarations file manager.hpp
+	
+	Copyright 2013 Valentin Palade 
+	vipalade@gmail.com
+
+	This file is part of SolidFrame framework.
+
+	SolidFrame is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	SolidFrame is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with SolidFrame.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SOLID_FRAME_MANAGER_HPP
+#define SOLID_FRAME_MANAGER_HPP
+
+#include "frame/common.hpp"
+#include "system/mutex.hpp"
+#include "utility/dynamicpointer.hpp"
+
+class  SpecificMapper;
+class  GlobalMapper;
+
+
+namespace solid{
+namespace frame{
+
+class	Service;
+class	Object;
+struct	Message;
+class	SchedulerBase;
+class	SelectorBase;
+
+typedef DynamicSharedPointer<Message>	MessageSharedPointerT;
+typedef DynamicPointer<Message>			MessagePointerT;
+
+typedef DynamicSharedPointer<Object>	ObjectSharedPointerT;
+typedef DynamicPointer<Object>			ObjectPointerT;
+
+class Manager{
+public:
+	static Manager& specific();
+	
+	Manager(
+		const size_t _svcprovisioncp = 1024,
+		int _objpermutbts = 6, int _mutrowsbts = 8, int _mutcolsbts = 8
+	);
+	
+	virtual ~Manager();
+
+	void stop();
+	
+	bool		registerService(Service &_rsvc);
+	ObjectUidT	registerObject(Object &_robj);
+	
+	void unregisterObject(Object &_robj);
+	
+	bool notify(ulong _sm, const ObjectUidT &_ruid);
+
+	bool notify(MessagePointerT &_rmsgptr, const ObjectUidT &_ruid);
+	
+	void raise(const Object &_robj);
+	
+	Mutex& mutex(const Object &_robj)const;
+	
+	Service& service(const Object &_robj)const;
+	
+	ObjectUidT  id(const Object &_robj)const;
+	
+	virtual SpecificMapper*  specificMapper();
+	
+	virtual GlobalMapper* globalMapper();
+	
+protected:
+	virtual void doPrepareThread();
+	virtual void doUnprepareThread();
+	size_t serviceCount()const;
+private:
+	friend class Service;
+	struct VisitContext{
+		
+	};
+	Mutex& serviceMutex(const Service &_rsvc);
+	ObjectUidT registerServiceObject(const Service &_rsvc, Object &_robj);
+	Object* nextServiceObject(const Service &_rsvc, VisitContext &_rctx);
+	
+	template <typename F>
+	bool forEachServiceObject(const Service &_rsvc, F _f){
+		Locker<Mutex>	lock(serviceMutex(_rsvc));
+		VisitContext	ctx;
+		Object			*pobj;
+		bool			signaled = false;
+		while((pobj = nextServiceObject(_rsvc, ctx)) != NULL){
+			_f(*pobj);
+			signaled = true;
+		}
+		return signaled;
+	}
+private:
+	struct Data;
+	Data	&d;
+};
+
+#ifndef NINLINES
+#include "frame/manager.ipp"
+#endif
+
+}//namespace frame
+}//namespace solid
+#endif
+
