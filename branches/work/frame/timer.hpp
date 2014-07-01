ef49b2694a6fd9559f0c8c4489e3ab58f2f0d292
vipalade <vipalade@0b79f258-eccb-fe0a-0eba-1848281d3a48>
2014-07-01 18:31:37 +0000

    new timer file

---
+++ branches/work/frame/timer.hpp
@@ -0,0 +1,25 @@
+// frame/timer.hpp
+//
+// Copyright (c) 2014 Valentin Palade (vipalade @ gmail . com) 
+//
+// This file is part of SolidFrame framework.
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt.
+//
+#ifndef SOLID_FRAME_TIMER_HPP
+#define SOLID_FRAME_TIMER_HPP
+
+#include "system/timespec.hpp"
+#include "frame/common.hpp"
+#include "frame/completion.hpp"
+
+namespace solid{
+namespace frame{
+
+class Timer: public CompletionHandler{
+
+};
+
+}//namespace frame
+}//namespace solid
