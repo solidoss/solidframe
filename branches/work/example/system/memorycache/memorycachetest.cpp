725686c44495cebef2e0815266fd16f10fdf091a
vipalade <vipalade@0b79f258-eccb-fe0a-0eba-1848281d3a48>
2014-10-14 18:47:47 +0000

    :( poor performance on memorycachetest compared with std allocator

--- branches/work/example/system/memorycache/memorycachetest.cpp
+++ branches/work/example/system/memorycache/memorycachetest.cpp
@@ -1,4 +1,6 @@
 #include "system/memorycache.hpp"
+#include <cstring>
+#include <vector>
 using namespace std;
 using namespace solid;
 
@@ -23,7 +25,26 @@ struct TestObject: BaseObject{
 
 int main(int argc, char *argv[]){
 	TestObject<4> *p4 = new TestObject<4>;
+	memset(p4->buf, 0, 4);
 	delete p4;
+	std::vector<TestObject<4>* > vec;
+	
+	size_t step = 1000;
+	size_t repeatcnt = 100;
+	size_t cp = repeatcnt * step;
+	vec.reserve(cp);
+	
+	size_t crtcp = 0;
+	for(size_t i = 0; i < repeatcnt; ++i){
+		crtcp += step;
+		for(size_t j = 0; j < crtcp; ++j){
+			vec.push_back(new TestObject<4>);
+		}
+		for(auto it = vec.begin(); it != vec.end(); ++it){
+			delete *it;
+		}
+		vec.clear();
+	}
 	return 0;
 }
 
