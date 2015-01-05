c46ca2d435a0e3b7b7c2f28514d9b6f32172ccdd
vipalade <vipalade@gmail.com>
2015-01-05 22:20:32 +0000

    new findmin study app

---
+++ example/frame/study/producer_consumer/findmin.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+template <size_t S, class A, class C>
+size_t find_min(A const &_ra, C _c);
+
+template <size_t S, class A, class C>
+size_t find_min(A const &_ra, C _c);
