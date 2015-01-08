60ba386c546cd7d071d0b9310c113c637dc8a1d9
vipalade <vipalade@0b79f258-eccb-fe0a-0eba-1848281d3a48>
2015-01-08 18:11:45 +0000

    keep working on a solution

--- branches/work/example/frame/study/producer_consumer/findmin.cpp
+++ branches/work/example/frame/study/producer_consumer/findmin.cpp
@@ -1,8 +1,173 @@
 #include <iostream>
+#include <array>
 using namespace std;
 
-template <size_t S, class A, class C>
-size_t find_min(A const &_ra, C _c);
 
-template <size_t S, class A, class C>
-size_t find_min(A const &_ra, C _c);
+template <size_t S>
+struct EmptyType{
+	enum{
+		value = S,
+	};
+};
+
+typedef std::pair<size_t, bool>		FindRetValT;
+
+template <class It>
+FindRetValT find_min_or_equal(It _it, EmptyType<1> _s){
+	return FindRetValT(0, true);
+}
+
+template <class It>
+FindRetValT find_min_or_equal(It _it, EmptyType<2> _s){
+	if(*_it < *(_it + 1)){
+		return FindRetValT(0, false);
+	} else if(*_it > *(_it + 1)){
+		return FindRetValT(1, false);
+	}
+	return FindRetValT(0, true);
+}
+
+template <class It, size_t S>
+FindRetValT find_min_or_equal(It _it, EmptyType<S> s){
+	
+	const FindRetValT	off1 = find_min_or_equal(_it, EmptyType<S/2>());
+	FindRetValT 		off2 = find_min_or_equal(_it + S/2, EmptyType<S - S/2>());
+	off2.first += S/2;
+	
+	if(*(_it + off1.first) < *(_it + off2.first)){
+		return FindRetValT(off1.first, false);
+	}else if(*(_it + off1.first) > *(_it + off2.first)){
+		return FindRetValT(off2.first, false);
+	}
+	return FindRetValT(off1.first, off1.second && off2.second);
+}
+
+template <class It, class C, size_t S>
+size_t find_min(It _it, C _c, EmptyType<S> s){
+	FindRetValT rv = find_min_or_equal(_it, s);
+	if(!rv.second){
+		return rv.first;
+	}else{
+		return _c();
+	}
+}
+
+
+template <class It, class Less>
+FindRetValT find_min_or_equal(It _it, Less const & _rless, EmptyType<1> _s){
+	return FindRetValT(0, true);
+}
+
+template <class It, class Less>
+FindRetValT find_min_or_equal(It _it, Less const & _rless, EmptyType<2> _s){
+	if(_rless(*_it, *(_it + 1))){
+		return FindRetValT(0, false);
+	} else if(_rless(*(_it + 1), *_it)){
+		return FindRetValT(1, false);
+	}
+	return FindRetValT(0, true);
+}
+
+template <class It, size_t S, class Less>
+FindRetValT find_min_or_equal(It _it, Less const &_rless, EmptyType<S> s){
+	
+	const FindRetValT	off1 = find_min_or_equal(_it, _rless, EmptyType<S/2>());
+	FindRetValT 		off2 = find_min_or_equal(_it + S/2, _rless, EmptyType<S - S/2>());
+	off2.first += S/2;
+	
+	if(_rless(*(_it + off1.first), *(_it + off2.first))){
+		return FindRetValT(off1.first, false);
+	}else if(_rless(*(_it + off2.first), *(_it + off1.first))){
+		return FindRetValT(off2.first, false);
+	}
+	return FindRetValT(off1.first, off1.second && off2.second);
+}
+
+template <class It, class C, class Less, size_t S>
+size_t find_min(It _it, C _c, Less const &_rless, EmptyType<S> s){
+	FindRetValT rv = find_min_or_equal(_it, _rless, s);
+	if(!rv.second){
+		return rv.first;
+	}else{
+		return _c();
+	}
+}
+
+template <class It>
+size_t find_min(It _it, EmptyType<1> _s){
+	return 0;
+}
+
+template <class It>
+size_t find_min(It _it, EmptyType<2> _s){
+	if(*_it < *(_it + 1)){
+		return 0;
+	}
+	return 1;
+}
+
+template <class It, size_t S>
+size_t find_min(It _it, EmptyType<S> s){
+	
+	const size_t	off1 = find_min(_it, EmptyType<S/2>());
+	const size_t	off2 = find_min(_it + S/2, EmptyType<S - S/2>()) + S/2;
+	
+	if(*(_it + off1) < *(_it + off2)){
+		return off1;
+	}
+	return off2;
+}
+
+
+
+template <class It, class Less>
+size_t find_min(It _it, Less const &_rless, EmptyType<1> _s){
+	return 0;
+}
+
+template <class It, class Less>
+size_t find_min(It _it, Less const &_rless, EmptyType<2> _s){
+	if(_rless(*_it, *(_it + 1))){
+		return 0;
+	}
+	return 1;
+}
+
+template <class It, class Less, size_t S>
+size_t find_min(It _it, Less const &_rless, EmptyType<S> s){
+	
+	const size_t	off1 = find_min(_it, EmptyType<S/2>());
+	const size_t	off2 = find_min(_it + S/2, EmptyType<S - S/2>()) + S/2;
+	
+	if(_rless(*(_it + off1), *(_it + off2))){
+		return off1;
+	}
+	return off2;
+}
+
+
+
+size_t ret_first(){
+	return -1;
+}
+
+bool less_cmp(const size_t _v1, const size_t _v2){
+	return _v1 < _v2;
+}
+
+int main(int argc, char *argv[]){
+	
+	array<int, 10>	arr1 = {112, 43, 74, 86, 101, 11};
+	array<int, 10>	arr2 = {112, 112, 112, 112, 112, 112};
+	
+	cout<<find_min(arr1.begin(), ret_first, EmptyType<6>())<<endl;
+	cout<<find_min(arr2.begin(), ret_first, EmptyType<6>())<<endl;
+	cout<<find_min(arr1.begin(), ret_first, less_cmp, EmptyType<6>())<<endl;
+	cout<<find_min(arr2.begin(), ret_first, less_cmp, EmptyType<6>())<<endl;
+	
+	cout<<find_min(arr1.begin(), EmptyType<6>())<<endl;
+	cout<<find_min(arr2.begin(), EmptyType<6>())<<endl;
+	cout<<find_min(arr1.begin(), less_cmp, EmptyType<6>())<<endl;
+	cout<<find_min(arr2.begin(), less_cmp, EmptyType<6>())<<endl;
+	return 0;
+}
--- branches/work/example/frame/study/producer_consumer/prodcons.cpp
+++ branches/work/example/frame/study/producer_consumer/prodcons.cpp
@@ -316,6 +316,14 @@ void Worker::run(){
 }//namespace baseline
 
 
+
+
+//=========================================================
+//	optim1::Scheduler
+//=========================================================
+namespace optim1{
+
+
 template <class C>
 size_t find_min(const std::array<C, 2> &_ra, size_t &_rcrtidx){
 	if(_ra[0] < _ra[1]){
@@ -446,11 +454,6 @@ size_t find_min(const std::array<C, 8> &_ra, size_t &_rcrtidx){
 	_rcrtidx = (idx + 1) % 8;
 	return idx;
 }
-
-//=========================================================
-//	optimized::Scheduler
-//=========================================================
-namespace optimized{
 
 typedef std::vector<TaskPtrT>	TaskVectorT;
 typedef std::queue<TaskPtrT>	TaskQueueT;
@@ -747,7 +750,443 @@ void Worker::run(){
 	cout<<"Worker exit - maxtsks = "<<maxtsks<<" maxqtsks = "<<maxqtsks<<" conscnt = "<<conscnt<<" loopcnt = "<<loopcnt<<endl;
 }
 
-}//namespace optimized
+}//namespace optim1
+
+
+
+//=========================================================
+//	optim1::Scheduler
+//=========================================================
+namespace optim2{
+
+
+template <class C>
+size_t find_min(const std::array<C, 2> &_ra, size_t &_rcrtidx){
+	if(_ra[0] < _ra[1]){
+		return 0;
+	}else if(_ra[0] > _ra[1]){
+		return 1;
+	}
+	const size_t idx = _rcrtidx;
+	_rcrtidx = (idx + 1) % 2;
+	return idx;
+}
+
+template <class C>
+size_t find_min(const std::array<C, 3> &_ra, size_t &_rcrtidx){
+	size_t	crtidx;
+	C		crtmin;
+	
+	if(_ra[0] < _ra[1]){
+		crtidx = 0;
+		crtmin = _ra[0];
+	}else if(_ra[0] > _ra[1]){
+		crtidx = 1;
+		crtmin = _ra[1];
+	}else if(_ra[1] == _ra[2]){
+		const size_t idx = _rcrtidx;
+		_rcrtidx = (idx + 1) % 3;
+		return idx;
+	}else{
+		crtidx = 0;
+		crtmin = _ra[0];
+	}
+	
+	if(crtmin < _ra[2]){
+		return crtidx;
+	}else{
+		return 2;
+	}
+	
+	
+}
+template <class C>
+size_t find_min(const std::array<C, 4> &_ra, size_t &_rcrtidx){
+#if 1
+	size_t	crtidx;
+	C		crtmin;
+	size_t	eqlcnt = 0;
+
+	if(_ra[0] == _ra[1] && _ra[1] == _ra[2] && _ra[2] == _ra[3]){
+		crtidx = _rcrtidx;
+		_rcrtidx = (crtidx + 1) % 4;
+	}else{
+		if(_ra[0] <= _ra[1]){
+			crtmin = _ra[0];
+			crtidx = 0;
+		}else{
+			crtmin = _ra[1];
+			crtidx = 1;
+		}
+		if(_ra[2] < crtmin){
+			crtmin = _ra[2];
+			crtidx = 2;
+		}
+		if(_ra[3] < crtmin){
+			crtidx = 3;
+		}
+	}
+	return crtidx;
+#else
+	size_t	crtidx;
+	C		crtmin;
+	size_t	eqlcnt = 0;
+	
+	if(_ra[0] < _ra[1]){
+		crtidx = 0;
+		crtmin = _ra[0];
+	}else if(_ra[0] > _ra[1]){
+		crtidx = 1;
+		crtmin = _ra[1];
+	}else{//==
+		crtidx = 0;
+		crtmin = _ra[0];
+		eqlcnt = 2;
+	}
+	
+	if(crtmin < _ra[2]){
+		
+	}else if(crtmin > _ra[2]){
+		crtmin = _ra[2];
+		crtidx = 2;
+	}else{//==
+		++eqlcnt;
+	}
+	
+	if(crtmin < _ra[3]){
+		return crtidx;
+	}else if(crtmin > _ra[3]){
+		return 3;
+	}else if(eqlcnt < 3){//==
+		return crtidx;
+	}
+		
+	crtidx = _rcrtidx;
+	_rcrtidx = (crtidx + 1) % 4;
+	return crtidx;
+#endif
+}
+template <class C>
+size_t find_min(const std::array<C, 5> &_ra, size_t &_rcrtidx){
+	const size_t idx = _rcrtidx;
+	_rcrtidx = (idx + 1) % 5;
+	return idx;
+}
+template <class C>
+size_t find_min(const std::array<C, 6> &_ra, size_t &_rcrtidx){
+	const size_t idx = _rcrtidx;
+	_rcrtidx = (idx + 1) % 6;
+	return idx;
+}
+template <class C>
+size_t find_min(const std::array<C, 7> &_ra, size_t &_rcrtidx){
+	const size_t idx = _rcrtidx;
+	_rcrtidx = (idx + 1) % 7;
+	return idx;
+}
+template <class C>
+size_t find_min(const std::array<C, 8> &_ra, size_t &_rcrtidx){
+	const size_t idx = _rcrtidx;
+	_rcrtidx = (idx + 1) % 8;
+	return idx;
+}
+
+typedef std::vector<TaskPtrT>	TaskVectorT;
+typedef std::queue<TaskPtrT>	TaskQueueT;
+
+class Scheduler;
+
+class Worker: public Thread{
+	friend class 	Scheduler;
+	Scheduler		&rsched;
+	TaskVectorT		tskvec[2];
+	Condition		cnd;
+	size_t			crtpushtskvecidx;
+	AtomicSizeT		crtpushsz;
+	AtomicSizeT		load;
+	
+	
+	Worker(Scheduler &_rsched):rsched(_rsched), crtpushtskvecidx(0), crtpushsz(0), load(0){}
+	
+	TaskVectorT* waitTasks(bool _peek, bool &_isrunning);
+	void run();
+	void schedule(TaskPtrT &_rtskptr);
+};
+
+
+typedef std::unique_ptr<Worker>		WorkerPtrT;
+typedef std::vector<WorkerPtrT>		WorkerVectorT;
+
+class Scheduler: public ::Scheduler{
+	enum Status{
+		StatusStoppedE = 0,
+		StatusRunningE,
+		StatusStoppingE,
+		StatusStoppingWaitE
+	};
+	friend class Worker;
+	Mutex			mtx;
+	Condition		cnd;
+	WorkerVectorT	wkrvec;
+	Status			status;
+	size_t			stopwaitcnt;
+	size_t			crtwkridx;
+	
+	~Scheduler();
+	virtual bool start(const size_t _conscnt);
+	virtual void schedule(TaskPtrT &_rtskptr);
+	virtual void stop(bool _wait);
+	size_t	computeScheduleWorkerIndex();
+public:
+	Scheduler():status(StatusStoppedE), stopwaitcnt(0), crtwkridx(0){}
+};
+
+Scheduler::~Scheduler(){
+	
+}
+/*virtual*/ bool Scheduler::start(const size_t _conscnt){
+	bool			err = false;
+	{
+		Locker<Mutex>	lock(mtx);
+		
+		if(status == StatusRunningE){
+			idbg("status == StatusRunningE");
+			return true;
+		}else if(status != StatusStoppedE || stopwaitcnt){
+			idbg("status != StatusStoppedE and stopwaitcnt = "<<stopwaitcnt);
+			do{
+				cnd.wait(lock);
+			}while(status != StatusStoppedE || stopwaitcnt);
+		}
+		
+		for(size_t i = 0; i < _conscnt; ++i){
+			WorkerPtrT	wkrptr(new Worker(*this));
+			if(wkrptr->start(false, false)){
+				wkrvec.push_back(std::move(wkrptr));
+			}else{
+				err = true;
+				break;
+			}
+		}
+		
+		if(err){
+			edbg("status = StatusStoppingWaitE");
+			status = StatusStoppingWaitE;
+		}
+	}
+	if(err){
+		edbg("before join all");
+		for(auto it = wkrvec.begin(); it != wkrvec.end(); ++it){
+			(*it)->join();
+		}
+		edbg("after join all");
+		Locker<Mutex>	lock(mtx);
+		status = StatusStoppedE;
+		cnd.broadcast();
+		idbg("status = StatusStoppedE");
+		return false;
+	}
+	status = StatusRunningE;
+	idbg("status = StatusRunningE");
+	return true;
+}
+
+inline size_t	Scheduler::computeScheduleWorkerIndex(){
+	switch(wkrvec.size()){
+		case 1:
+			return 0;
+		case 2:{
+			const std::array<size_t, 2> arr = {
+				wkrvec[0]->load, wkrvec[1]->load
+			};
+			return find_min(arr, crtwkridx);
+		}
+		case 3:{
+			const std::array<size_t, 3> arr = {
+				wkrvec[0]->load, wkrvec[1]->load, wkrvec[2]->load
+			};
+			return find_min(arr, crtwkridx);
+		}
+		case 4:{
+			const std::array<size_t, 4> arr = {
+				wkrvec[0]->load, wkrvec[1]->load, wkrvec[2]->load, wkrvec[3]->load
+			};
+			return find_min(arr, crtwkridx);
+		}
+		case 5:{
+			const std::array<size_t, 5> arr = {
+				wkrvec[0]->load, wkrvec[1]->load, wkrvec[2]->load, wkrvec[3]->load,
+				wkrvec[4]->load
+			};
+			return find_min(arr, crtwkridx);
+		}
+		case 6:{
+			const std::array<size_t, 6> arr = {
+				wkrvec[0]->load, wkrvec[1]->load, wkrvec[2]->load, wkrvec[3]->load,
+				wkrvec[4]->load, wkrvec[5]->load
+			};
+			return find_min(arr, crtwkridx);
+		}
+		case 7:{
+			const std::array<size_t, 7> arr = {
+				wkrvec[0]->load, wkrvec[1]->load, wkrvec[2]->load, wkrvec[3]->load,
+				wkrvec[4]->load, wkrvec[5]->load, wkrvec[6]->load
+			};
+			return find_min(arr, crtwkridx);
+		}
+		case 8:{
+			const std::array<size_t, 8> arr = {
+				wkrvec[0]->load, wkrvec[1]->load, wkrvec[2]->load, wkrvec[3]->load,
+				wkrvec[4]->load, wkrvec[5]->load, wkrvec[6]->load, wkrvec[7]->load
+			};
+			return find_min(arr, crtwkridx);
+		}
+		default:
+			break;
+	}
+	
+	const size_t	cwi = crtwkridx;
+	crtwkridx = (cwi + 1) % wkrvec.size();
+	return cwi;
+}
+
+/*virtual*/ void Scheduler::schedule(TaskPtrT &_rtskptr){
+	Locker<Mutex>	lock(mtx);
+	if(status == StatusRunningE){
+		wkrvec[computeScheduleWorkerIndex()]->schedule(_rtskptr);
+	}
+}
+/*virtual*/ void Scheduler::stop(bool _wait){
+	{
+		Locker<Mutex>	lock(mtx);
+		if(status == StatusRunningE){
+			idbg("status == StatusRunningE");
+			status = _wait ? StatusStoppingWaitE : StatusStoppingE;
+			for(auto it = wkrvec.begin(); it != wkrvec.end(); ++it){
+				(*it)->cnd.signal();
+			}
+		}else if(status == StatusStoppingE){
+			idbg("status == StatusStoppingE");
+			status = _wait ? StatusStoppingWaitE : StatusStoppingE;
+		}else if(status == StatusStoppingWaitE){
+			idbg("status == StatusStoppingWaitE and _wait = "<<_wait);
+			if(_wait){
+				++stopwaitcnt;
+				do{
+					cnd.wait(lock);
+				}while(status != StatusStoppedE);
+				--stopwaitcnt;
+				cnd.signal();
+			}
+			return;
+		}else if(status == StatusStoppedE){
+			idbg("status == StatusStoppedE");
+			return;
+		}
+	}
+	if(_wait){
+		for(auto it = wkrvec.begin(); it != wkrvec.end(); ++it){
+			(*it)->join();
+		}
+		Locker<Mutex>	lock(mtx);
+		status = StatusStoppedE;
+		idbg("status = StatusStoppedE");
+		cnd.broadcast();
+	}
+}
+
+//---------------------------------------------------------
+TaskVectorT* Worker::waitTasks(bool _peek, bool &_risrunning){
+	if(_peek && crtpushsz == 0){
+		return NULL;
+	}
+	Locker<Mutex>	lock(rsched.mtx);
+	crtpushsz = 0;
+	if(rsched.status <= Scheduler::StatusRunningE){
+		if(tskvec[crtpushtskvecidx].empty()){
+			if(_peek){
+				return NULL;
+			}else{
+				do{
+					cnd.wait(lock);
+				}while(tskvec[crtpushtskvecidx].empty() && rsched.status <= Scheduler::StatusRunningE);
+			}
+		}
+	}else{
+		_risrunning = false;
+	}
+	if(tskvec[crtpushtskvecidx].size()){
+		TaskVectorT *pvec = &tskvec[crtpushtskvecidx];
+		crtpushtskvecidx = (crtpushtskvecidx + 1) % 2;
+		return pvec;
+	}
+	return NULL;
+}
+void Worker::schedule(TaskPtrT &_rtskptr){
+	++crtpushsz;
+	++load;
+	tskvec[crtpushtskvecidx].push_back(std::move(_rtskptr));
+	if(tskvec[crtpushtskvecidx].size() == 1){
+		cnd.signal();
+	}
+}
+void Worker::run(){
+	vdbg("Worker enter");
+	Context		ctx;
+	TaskVectorT	*ptv = NULL;
+	size_t		maxtsks = 0;
+	size_t		maxqtsks = 0;
+	size_t		conscnt = 0;
+	size_t		loopcnt = 0;
+	TaskQueueT	tskq;
+	
+	bool		isrunning = true;
+	
+	size_t		opcnt = 0;
+	
+	do{
+		const size_t	tskqsz = tskq.size();
+		if(tskqsz > maxqtsks){
+			maxqtsks = tskqsz;
+		}
+		loopcnt += tskqsz;
+		consloopcnt += tskqsz;
+		opcnt += tskqsz;
+		
+		size_t			qcnt = tskqsz;
+		while(qcnt--){
+			if(tskq.front()->run(ctx)){
+				tskq.push(std::move(tskq.front()));
+				tskq.pop();
+			}else{
+				tskq.pop();
+ 				--load;
+			}
+		}
+		//load -= (tskqsz - tskq.size());
+		ptv = waitTasks(!tskq.empty(), isrunning);
+		if(ptv){
+			if(ptv->size() > maxtsks){
+				maxtsks = ptv->size();
+			}
+			conscnt += ptv->size();
+			loopcnt += ptv->size();
+			constskcnt += ptv->size();
+			
+			for(auto it = ptv->begin(); it != ptv->end(); ++it){
+				tskq.push(std::move(*it));
+			}
+			ptv->clear();
+		}
+		ctx.ct.currentMonotonic();
+		load = tskq.size();
+	}while(tskq.size() || isrunning);
+	
+	vdbg("Worker exit - maxtsks = "<<maxtsks<<" maxqtsks = "<<maxqtsks<<" conscnt = "<<conscnt<<" loopcnt = "<<loopcnt);
+	cout<<"Worker exit - maxtsks = "<<maxtsks<<" maxqtsks = "<<maxqtsks<<" conscnt = "<<conscnt<<" loopcnt = "<<loopcnt<<endl;
+}
+
+}//namespace optim2
 
 
 //=========================================================
@@ -819,15 +1258,18 @@ typedef std::vector<ProducerPtrT>	ProducerVectorT;
 enum SchedulerType{
 	SchedulerDummyE,
 	SchedulerBaselineE,
-	SchedulerOptimizedE,
+	SchedulerOptim1E,
+	SchedulerOptim2E,
 };
 
 Scheduler * schedulerFactory(const SchedulerType _tp){
 	switch(_tp){
 		case SchedulerBaselineE:
 			return new baseline::Scheduler;
-		case SchedulerOptimizedE:
-			return new optimized::Scheduler;
+		case SchedulerOptim1E:
+			return new optim1::Scheduler;
+		case SchedulerOptim2E:
+			return new optim2::Scheduler;
 		default:
 			return new Scheduler;
 	}
@@ -851,9 +1293,11 @@ int main(int argc, char *argv[]){
 			case 'B':
 				schedtype = SchedulerBaselineE;
 				break;
-			case 'o':
-			case 'O':
-				schedtype = SchedulerOptimizedE;
+			case '1':
+				schedtype = SchedulerOptim1E;
+				break;
+			case '2':
+				schedtype = SchedulerOptim2E;
 				break;
 			default:
 				cout<<"Unknown scheduler type choice: "<<argv[1][0]<<endl;
