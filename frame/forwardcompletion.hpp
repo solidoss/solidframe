1cebb38b8cf2a1bee6b9d26c8e08fd1abaf3ad11
Valentin Palade <vipalade@gmail.com>
2015-03-15 14:01:28 +0200

    working on frame fileserver example

---
+++ frame/forwardcompletion.hpp
@@ -0,0 +1,27 @@
+// frame/aio/forwardcompletion.hpp
+//
+// Copyright (c) 2015 Valentin Palade (vipalade @ gmail . com) 
+//
+// This file is part of SolidFrame framework.
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt.
+//
+#ifndef SOLID_FRAME_FORWARD_COMPLETION_HPP
+#define SOLID_FRAME_FORWARD_COMPLETION_HPP
+
+namespace solid{
+namespace frame{
+
+class CompletionHandler;
+
+struct ForwardCompletionHandler{
+	ForwardCompletionHandler(CompletionHandler *_pch = nullptr):pnext(_pch){}
+	CompletionHandler	*pnext;
+};
+
+}//namespace frame
+}//namespace solid
+
+
+#endif
---
+++ frame/reactorcontext.hpp
@@ -0,0 +1,100 @@
+// frame/aio/reactorcontext.hpp
+//
+// Copyright (c) 2015 Valentin Palade (vipalade @ gmail . com) 
+//
+// This file is part of SolidFrame framework.
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt.
+//
+#ifndef SOLID_FRAME_REACTORCONTEXT_HPP
+#define SOLID_FRAME_REACTORCONTEXT_HPP
+
+#include "system/common.hpp"
+#include "system/error.hpp"
+#include "system/socketdevice.hpp"
+#include "system/timespec.hpp"
+
+#include "frame/event.hpp"
+#include "frame/aio/aiocommon.hpp"
+
+namespace solid{
+namespace frame{
+
+class Service;
+class Object;
+class Reactor;
+class CompletionHandler;
+
+struct ReactorContext{
+	~ReactorContext(){
+		
+	}
+	
+	const TimeSpec& time()const{
+		return rcrttm;
+	}
+	
+	ERROR_NS::error_code const& systemError()const{
+		return syserr;
+	}
+	
+	ERROR_NS::error_condition const& error()const{
+		return err;
+	}
+	
+	Object& object()const;
+	Service& service()const;
+	
+	UidT objectUid()const;
+	
+	void clearError(){
+		err.clear();
+		syserr.clear();
+	}
+private:
+	friend class CompletionHandler;
+	friend class Reactor;
+	friend class Object;
+	
+	Reactor& reactor(){
+		return rreactor;
+	}
+	
+	Reactor const& reactor()const{
+		return rreactor;
+	}
+	ReactorEventsE reactorEvent()const{
+		return reactevn;
+	}
+	CompletionHandler* completionHandler()const;
+	
+	
+	void error(ERROR_NS::error_condition const& _err){
+		err = _err;
+	}
+	
+	void systemError(ERROR_NS::error_code const& _err){
+		syserr = _err;
+	}
+	
+	ReactorContext(
+		Reactor	&_rreactor,
+		const TimeSpec &_rcrttm
+	):	rreactor(_rreactor),
+		rcrttm(_rcrttm), chnidx(-1), objidx(-1), reactevn(ReactorEventNone){}
+	
+	Reactor						&rreactor;
+	const TimeSpec				&rcrttm;
+	size_t						chnidx;
+	size_t						objidx;
+	ReactorEventsE				reactevn;
+	ERROR_NS::error_code		syserr;
+	ERROR_NS::error_condition	err;
+};
+
+}//namespace frame
+}//namespace solid
+
+
+#endif
