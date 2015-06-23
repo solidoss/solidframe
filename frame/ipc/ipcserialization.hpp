8050094d5bc1b60dcf90328b7c873bf9783d246f
valentin palade <vipalade@gmail.com>
2015-06-23 17:37:30 +0300

    keep working on ipc protocol

---
+++ frame/ipc/ipcserialization.hpp
@@ -0,0 +1,43 @@
+// frame/ipc/ipcserialization.hpp
+//
+// Copyright (c) 2015 Valentin Palade (vipalade @ gmail . com) 
+//
+// This file is part of SolidFrame framework.
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt.
+//
+#ifndef SOLID_FRAME_IPC_IPCSERIALIZATION_HPP
+#define SOLID_FRAME_IPC_IPCSERIALIZATION_HPP
+
+
+#include "ipcmessage.hpp"
+#include "serialization/binary.hpp"
+#include "system/function.hpp"
+
+
+namespace solid{
+namespace frame{
+namespace ipc{
+
+struct ConnectionContext;
+
+typedef FUNCTION<void(ConnectionContext &, MessagePointerT &)>							MessageReceiveFunctionT;
+typedef FUNCTION<void(ConnectionContext &, MessagePointerT &, ErrorConditionT const &)>	MessageCompleteFunctionT;
+typedef FUNCTION<uint32(ConnectionContext &, Message const &)>							MessagePrepareFunctionT;
+
+struct TypeStub{
+	MessagePrepareFunctionT		prepare_fnc;
+	MessageReceiveFunctionT		receive_fnc;
+	MessageCompleteFunctionT	complete_fnc;
+};
+
+typedef serialization::binary::Serializer<ConnectionContext>						SerializerT;
+typedef serialization::binary::Deserializer<ConnectionContext>						DeserializerT;
+typedef serialization::TypeIdMap<SerializerT, DeserializerT, TypeStub>				TypeIdMapT;
+
+}//namespace ipc
+}//namespace frame
+}//namespace solid
+
+#endif
