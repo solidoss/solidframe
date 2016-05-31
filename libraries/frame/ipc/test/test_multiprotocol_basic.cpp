db0a6c974d2a1bcb8698303cb18bac5ab090fcaf
Valentin Palade <vipalade@gmail.com>
2016-05-31 18:30:22 +0300

    ipc: start multiprotocol_basic test

---
+++ libraries/frame/ipc/test/test_multiprotocol_basic.cpp
@@ -0,0 +1,203 @@
+#include "frame/manager.hpp"
+#include "frame/scheduler.hpp"
+#include "frame/service.hpp"
+
+#include "frame/aio/aioreactor.hpp"
+#include "frame/aio/aioobject.hpp"
+#include "frame/aio/aiolistener.hpp"
+#include "frame/aio/aiotimer.hpp"
+#include "frame/aio/aioresolver.hpp"
+
+#include "frame/aio/openssl/aiosecurecontext.hpp"
+#include "frame/aio/openssl/aiosecuresocket.hpp"
+
+#include "frame/ipc/ipcservice.hpp"
+#include "frame/ipc/ipcconfiguration.hpp"
+#include "frame/ipc/ipcprotocol_serialization_v1.hpp"
+
+
+#include "system/thread.hpp"
+#include "system/mutex.hpp"
+#include "system/condition.hpp"
+#include "system/exception.hpp"
+
+#include "system/debug.hpp"
+
+#include <iostream>
+
+using namespace std;
+using namespace solid;
+
+typedef frame::Scheduler<frame::aio::Reactor>	AioSchedulerT;
+typedef frame::aio::openssl::Context			SecureContextT;
+
+
+namespace{
+
+std::string						pattern;
+bool							running = true;
+Mutex							mtx;
+Condition						cnd;
+
+size_t real_size(size_t _sz){
+	//offset + (align - (offset mod align)) mod align
+	return _sz + ((sizeof(uint64) - (_sz % sizeof(uint64))) % sizeof(uint64));
+}
+
+void server_connection_stop(frame::ipc::ConnectionContext &_rctx, ErrorConditionT const&){
+	idbg(_rctx.recipientId());
+}
+
+void server_connection_start(frame::ipc::ConnectionContext &_rctx){
+	idbg(_rctx.recipientId());
+}
+
+void server_complete_message(
+	frame::ipc::ConnectionContext &_rctx,
+	DynamicPointer<Message> &_rsent_msg_ptr, DynamicPointer<Message> &_rrecv_msg_ptr,
+	ErrorConditionT const &_rerror
+){
+	if(_rrecv_msg_ptr.get()){
+		idbg(_rctx.recipientId()<<" received message with id on sender "<<_rrecv_msg_ptr->requestId());
+	}
+	if(_rsent_msg_ptr.get()){
+		idbg(_rctx.recipientId()<<" done sent message "<<_rsent_msg_ptr.get());
+	}
+}
+
+}//namespace
+
+
+
+int test_multiprotocol_basic(int argc, char **argv){
+	Thread::init();
+#ifdef SOLID_HAS_DEBUG
+	Debug::the().levelMask("view");
+	Debug::the().moduleMask("frame_ipc:view any:view");
+	Debug::the().initStdErr(false, nullptr);
+	//Debug::the().initFile("test_clientserver_basic", false);
+#endif
+	
+	size_t max_per_pool_connection_count = 1;
+	
+	if(argc > 1){
+		max_per_pool_connection_count = atoi(argv[1]);
+		if(max_per_pool_connection_count == 0){
+			max_per_pool_connection_count = 1;
+		}
+		if(max_per_pool_connection_count > 100){
+			max_per_pool_connection_count = 100;
+		}
+	}
+	
+	for(int j = 0; j < 1; ++j){
+		for(int i = 0; i < 127; ++i){
+			int c = (i + j) % 127;
+			if(isprint(c) and !isblank(c)){
+				pattern += static_cast<char>(c);
+			}
+		}
+	}
+	
+	size_t	sz = real_size(pattern.size());
+	
+	if(sz > pattern.size()){
+		pattern.resize(sz - sizeof(uint64));
+	}else if(sz < pattern.size()){
+		pattern.resize(sz);
+	}
+	{
+		AioSchedulerT			sch_client;
+		AioSchedulerT			sch_server;
+			
+			
+		frame::Manager			m;
+		frame::ipc::Service		ipcserver(m);
+		frame::ipc::Service		ipcclient(m);
+		ErrorConditionT			err;
+		
+		frame::aio::Resolver	resolver;
+		
+		err = sch_client.start(1);
+		
+		if(err){
+			edbg("starting aio client scheduler: "<<err.message());
+			return 1;
+		}
+		
+		err = sch_server.start(1);
+		
+		if(err){
+			edbg("starting aio server scheduler: "<<err.message());
+			return 1;
+		}
+		
+		err = resolver.start(1);
+		
+		if(err){
+			edbg("starting aio resolver: "<<err.message());
+			return 1;
+		}
+		
+		std::string		server_port;
+		
+		{//ipc server initialization
+			frame::ipc::serialization_v1::Protocol	*proto = new frame::ipc::serialization_v1::Protocol;
+			frame::ipc::Configuration				cfg(sch_server, proto);
+			
+			proto->registerType<Message>(
+				serialization::basic_factory<Message>,
+				server_complete_message
+			);
+			
+			//cfg.recv_buffer_capacity = 1024;
+			//cfg.send_buffer_capacity = 1024;
+			
+			cfg.connection_stop_fnc = server_connection_stop;
+			cfg.connection_start_incoming_fnc = server_connection_start;
+			
+			
+			cfg.connection_start_state = frame::ipc::ConnectionState::Active;
+			cfg.listener_address_str = "0.0.0.0:0";
+			
+			err = ipcserver.reconfigure(std::move(cfg));
+			
+			if(err){
+				edbg("starting server ipcservice: "<<err.message());
+				Thread::waitAll();
+				return 1;
+			}
+			
+			{
+				std::ostringstream oss;
+				oss<<ipcserver.configuration().listenerPort();
+				server_port = oss.str();
+				idbg("server listens on port: "<<server_port);
+			}
+		}
+		
+			
+		Locker<Mutex>	lock(mtx);
+		
+		while(running){
+			//cnd.wait(lock);
+			TimeSpec	abstime = TimeSpec::createRealTime();
+			abstime += (120 * 1000);//ten seconds
+			cnd.wait(lock);
+			bool b = true;//cnd.wait(lock, abstime);
+			if(!b){
+				//timeout expired
+				SOLID_THROW("Process is taking too long.");
+			}
+		}
+		
+		m.stop();
+	}
+	
+	Thread::waitAll();
+	
+	std::cout<<"Transfered size = "<<(transfered_size * 2)/1024<<"KB"<<endl;
+	std::cout<<"Transfered count = "<<transfered_count<<endl;
+	std::cout<<"Connection count = "<<connection_count<<endl;
+	return 0;
+}
