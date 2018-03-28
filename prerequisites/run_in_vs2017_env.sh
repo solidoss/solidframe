027408265cb1d0836d8b73e252b9e864727048e9
Valentin Palade <valentin@yalos-solutions.com>
2018-03-28 14:42:21 +0300

    windows: update run in VS env scripts

--- prerequisites/run_in_vs2017_env.sh
+++ prerequisites/run_in_vs2017_env.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+echo "*******************************************"
+echo ""
+echo "Usage:"
+echo "$0 amd64|x86 bash"
+echo ""
+echo "*******************************************"
 
 VSCMDPATH="`find c\:/Program\ Files\ \(x86\)/ -name vcvarsall.bat 2>/dev/null | grep -v -F "Permission" | grep -F "2017"`"
 VSSETUP="`cygpath -w "$VSCMDPATH"`"
