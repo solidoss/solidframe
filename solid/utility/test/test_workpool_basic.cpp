7e23542befb8b2e8135d0464bbac068c8e14dd61
Valentin Palade <vipalade@gmail.com>
2018-10-19 13:31:22 +0300

    work: fix test_workpool_basic broken by previous commit

--- solid/utility/test/test_workpool_basic.cpp
+++ solid/utility/test/test_workpool_basic.cpp
@@ -16,32 +16,38 @@ const LoggerT logger("test_basic");
 int test_workpool_basic(int /*argc*/, char* /*argv*/ [])
 {
     solid::log_start(std::cerr, {".*:EWS", "test_basic:VIEW"});
-    using WorkPoolT                  = WorkPool<size_t>;
+    using WorkPoolT  = WorkPool<size_t>;
+    using AtomicPWPT = std::atomic<WorkPoolT*>;
+
     const int           wait_seconds = 100;
     const size_t        cnt{5000000};
     std::atomic<size_t> val{0};
     promise<void>       prom;
-    WorkPoolT           wp{
-        2,
-        WorkPoolConfiguration(),
-        [&val](size_t _v) {
-            val += _v;
-        }};
-    thread wait_thread(
-        [](promise<void>& _rprom, WorkPoolT& _rwp, const int _wait_time_seconds) {
+    AtomicPWPT          pwp{nullptr};
+    thread              wait_thread(
+        [](promise<void>& _rprom, AtomicPWPT& _rpwp, const int _wait_time_seconds) {
             if (_rprom.get_future().wait_for(chrono::seconds(_wait_time_seconds)) != future_status::ready) {
-
-                _rwp.dumpStatistics();
+                if (_rpwp != nullptr) {
+                    _rpwp.load()->dumpStatistics();
+                }
                 solid_throw(" Test is taking too long - waited " << _wait_time_seconds << " secs");
             }
         },
-        std::ref(prom), std::ref(wp), wait_seconds);
+        std::ref(prom), std::ref(pwp), wait_seconds);
 
     {
+        WorkPoolT wp{
+            2,
+            WorkPoolConfiguration(),
+            [&val](size_t _v) {
+                val += _v;
+            }};
+        pwp = &wp;
         solid_log(logger, Verbose, "before loop");
         for (size_t i = 0; i < cnt; ++i) {
             wp.push(i);
         };
+        pwp = nullptr;
         solid_log(logger, Verbose, "after loop");
     }
     prom.set_value();
