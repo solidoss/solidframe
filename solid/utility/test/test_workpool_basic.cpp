99882dcf6bf7de65d6f5d1b40b0c12ac30e9822f
Valentin Palade <vipalade@gmail.com>
2018-10-19 14:17:33 +0300

    work: loop more on test_workpool_basic

--- solid/utility/test/test_workpool_basic.cpp
+++ solid/utility/test/test_workpool_basic.cpp
@@ -19,7 +19,8 @@ int test_workpool_basic(int /*argc*/, char* /*argv*/ [])
     using WorkPoolT  = WorkPool<size_t>;
     using AtomicPWPT = std::atomic<WorkPoolT*>;
 
-    const int           wait_seconds = 100;
+    const int           wait_seconds = 500;
+    const int           loop_cnt = 10;
     const size_t        cnt{5000000};
     std::atomic<size_t> val{0};
     promise<void>       prom;
@@ -35,7 +36,7 @@ int test_workpool_basic(int /*argc*/, char* /*argv*/ [])
         },
         std::ref(prom), std::ref(pwp), wait_seconds);
 
-    {
+    for(int i = 0; i < loop_cnt; ++i){
         WorkPoolT wp{
             2,
             WorkPoolConfiguration(),
@@ -55,7 +56,7 @@ int test_workpool_basic(int /*argc*/, char* /*argv*/ [])
     wait_thread.join();
     solid_log(logger, Verbose, "after wait thread join: val = " << val);
 
-    const size_t v = ((cnt - 1) * cnt) / 2;
+    const size_t v = (loop_cnt * ((cnt - 1) * cnt)) / 2;
 
     solid_check(v == val);
     return 0;
