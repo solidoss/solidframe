760bdd7947f50bfc2085daf9e36ff7e8bf6d48ed
Palade Valentin <vipalade@gmail.com>
2019-08-12 15:57:10 +0300

    Update build_openssl.cmake

--- cmake/build_openssl.cmake
+++ cmake/build_openssl.cmake
@@ -13,7 +13,7 @@ if(SOLID_ON_WINDOWS)
         DOWNLOAD_NO_PROGRESS OFF
         #URL_MD5 "6f4571e7c5a66ccc3323da6c24be8f05"
         #CONFIGURE_COMMAND ${EXTERNAL_DIR}/openssl/src/build-openssl/config --prefix=${EXTERNAL_DIR} --openssldir=ssl_
-        CONFIGURE_COMMAND C:/Strawberry/perl/bin/perl Configure ${OPENSSL_TARGET} --prefix=${EXTERNAL_DIR} --openssldir=ssl_
+        CONFIGURE_COMMAND C:/Strawberry/perl/bin/perl Configure ${OPENSSL_TARGET} --prefix=${EXTERNAL_DIR} --openssldir=ssl_ no-shared
         BUILD_COMMAND nmake
         INSTALL_COMMAND nmake install_sw
         BUILD_IN_SOURCE 1
