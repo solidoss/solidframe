870f2ddf28f01abe402320677ae27930be220c87
Valentin Palade <vipalade@gmail.com>
2019-09-09 23:35:20 +0300

    work: working on fixing openssl build

--- cmake/build_openssl.cmake
+++ cmake/build_openssl.cmake
@@ -13,7 +13,7 @@ if(SOLID_ON_WINDOWS)
         DOWNLOAD_NO_PROGRESS OFF
         #URL_MD5 "6f4571e7c5a66ccc3323da6c24be8f05"
         #CONFIGURE_COMMAND ${EXTERNAL_DIR}/openssl/src/build-openssl/config --prefix=${EXTERNAL_DIR} --openssldir=ssl_
-        CONFIGURE_COMMAND C:/Strawberry/perl/bin/perl Configure ${OPENSSL_TARGET} --prefix=${EXTERNAL_DIR} no-shared no-stdio
+        CONFIGURE_COMMAND C:/Strawberry/perl/bin/perl Configure ${OPENSSL_TARGET} --prefix=${EXTERNAL_DIR} no-shared no-unit-test no-tests no-ui
         BUILD_COMMAND nmake
         INSTALL_COMMAND nmake install_sw
         BUILD_IN_SOURCE 1
@@ -24,7 +24,7 @@ if(SOLID_ON_WINDOWS)
     )
 
     set(OPENSSL_FOUND TRUE)
-    set(OPENSSL_LIBRARIES libcrypto libssl)
+    set(OPENSSL_LIBRARIES libssl libcrypto)
 else()
     ExternalProject_Add(
         build-openssl
@@ -33,9 +33,9 @@ else()
         URL https://www.openssl.org/source/openssl-1.1.1c.tar.gz
         DOWNLOAD_NO_PROGRESS ON
         #URL_MD5 "6f4571e7c5a66ccc3323da6c24be8f05"
-        CONFIGURE_COMMAND ${EXTERNAL_DIR}/openssl/src/build-openssl/config --prefix=${EXTERNAL_DIR} no-shared no-stdio
+        CONFIGURE_COMMAND ${EXTERNAL_DIR}/openssl/src/build-openssl/config --prefix=${EXTERNAL_DIR} no-shared no-unit-test no-tests no-ui
         BUILD_COMMAND make
-        INSTALL_COMMAND make install_sw
+        INSTALL_COMMAND make -j4 install_sw
         BUILD_IN_SOURCE 1
         LOG_UPDATE ON
         LOG_CONFIGURE ON
@@ -44,7 +44,7 @@ else()
     )
 
     set(OPENSSL_FOUND TRUE)
-    set(OPENSSL_LIBRARIES crypto ssl)
+    set(OPENSSL_LIBRARIES ssl crypto)
 endif()
 
 
