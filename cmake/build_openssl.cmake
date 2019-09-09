23abe0903dd0f0f9385f0cc712bcfad59f77e3a0
Valentin Palade <vipalade@gmail.com>
2019-09-09 21:40:30 +0300

    work: working on minimizing openssl build time

--- cmake/build_openssl.cmake
+++ cmake/build_openssl.cmake
@@ -13,7 +13,7 @@ if(SOLID_ON_WINDOWS)
         DOWNLOAD_NO_PROGRESS OFF
         #URL_MD5 "6f4571e7c5a66ccc3323da6c24be8f05"
         #CONFIGURE_COMMAND ${EXTERNAL_DIR}/openssl/src/build-openssl/config --prefix=${EXTERNAL_DIR} --openssldir=ssl_
-        CONFIGURE_COMMAND C:/Strawberry/perl/bin/perl Configure ${OPENSSL_TARGET} --prefix=${EXTERNAL_DIR} --openssldir=ssl_ no-shared
+        CONFIGURE_COMMAND C:/Strawberry/perl/bin/perl Configure ${OPENSSL_TARGET} --prefix=${EXTERNAL_DIR} --openssldir=ssl_ no-shared no-shared no-stdio
         BUILD_COMMAND nmake
         INSTALL_COMMAND nmake install_sw
         BUILD_IN_SOURCE 1
@@ -33,7 +33,7 @@ else()
         URL https://www.openssl.org/source/openssl-1.1.1c.tar.gz
         DOWNLOAD_NO_PROGRESS ON
         #URL_MD5 "6f4571e7c5a66ccc3323da6c24be8f05"
-        CONFIGURE_COMMAND ${EXTERNAL_DIR}/openssl/src/build-openssl/config --prefix=${EXTERNAL_DIR} --openssldir=ssl_
+        CONFIGURE_COMMAND ${EXTERNAL_DIR}/openssl/src/build-openssl/config --prefix=${EXTERNAL_DIR} no-shared no-stdio
         BUILD_COMMAND make
         INSTALL_COMMAND make install_sw
         BUILD_IN_SOURCE 1
