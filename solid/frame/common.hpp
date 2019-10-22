27e8fccd1b29e12bad8555f84addb4d4d2ff895d
Valentin Palade <vipalade@gmail.com>
2019-10-22 20:11:24 +0300

    work: copyright and license updates

--- solid/frame/common.hpp
+++ solid/frame/common.hpp
@@ -5,7 +5,7 @@
 // This file is part of SolidFrame framework.
 //
 // Distributed under the Boost Software License, Version 1.0.
-// See accompanying file LICENSE_1_0.txt || copy at http://www.boost.org/LICENSE_1_0.txt.
+// See accompanying file LICENSE.txt || copy at http://www.boost.org/LICENSE_1_0.txt.
 //
 
 #pragma once
--- solid/utility/src/memoryfile.cpp
+++ solid/utility/src/memoryfile.cpp
@@ -5,7 +5,7 @@
 // This file is part of SolidFrame framework.
 //
 // Distributed under the Boost Software License, Version 1.0.
-// See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt.
+// See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt.
 //
 #include <cerrno>
 #include <cstring>
