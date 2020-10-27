20b0a4ae5a92bd003d48b3ee4a36b9f53d09cab0
Valentin Palade <vipalade@gmail.com>
2020-10-27 21:01:45 +0200

    improving build system

--- cmake/find_openssl.cmake
+++ cmake/find_openssl.cmake
@@ -1,7 +1,3 @@
-if(ON_ANDROID)
-    set(OPENSSL_ROOT_DIR "${EXTERNAL_DIR}/${ANDROID_ABI}")
-else()
-    set(OPENSSL_ROOT_DIR ${EXTERNAL_DIR})
-endif()
+set(OPENSSL_ROOT_DIR ${EXTERNAL_DIR})
 
 find_package(OpenSSL)
