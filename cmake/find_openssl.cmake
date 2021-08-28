57ff23f10f6dd1b93e881f4490c15f0d27119959
Valentin Palade <vipalade@gmail.com>
2021-08-28 15:53:26 +0300

    event: windows: cmake: copy openssl dlls on test targets dirs

--- cmake/find_openssl.cmake
+++ cmake/find_openssl.cmake
@@ -2,6 +2,13 @@ set(OPENSSL_ROOT_DIR ${EXTERNAL_DIR})
 
 find_package(OpenSSL)
 
+if(SOLID_ON_WINDOWS)
+    file( GLOB OPENSSL_SSL_DLL "${EXTERNAL_DIR}/bin/libssl-*.dll")
+    file( GLOB OPENSSL_CRYPTO_DLL "${EXTERNAL_DIR}/bin/libcrypto-*.dll")
+
+    message("OpenSSL DLLs ${OPENSSL_SSL_DLL} ${OPENSSL_CRYPTO_DLL}")
+endif()
+
 if(OPENSSL_FOUND)
     message("\n-- OpenSSL version: ${OPENSSL_VERSION}\n")
 else()
