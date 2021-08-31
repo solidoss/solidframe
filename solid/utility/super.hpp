11be5298a1cdfa7020b5b2278cc79d85a1101367
Valentin Palade <vipalade@gmail.com>
2021-08-31 16:37:07 +0300

    work: readded utility/super

---
+++ solid/utility/super.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <functional>
+#include <typeindex>
+
+namespace solid {
+using SuperFindFunctionT = std::function<bool(const std::type_index&, size_t&)>;
+
+struct SuperBase {
+    virtual ~SuperBase();
+    virtual bool superFind(std::type_index& _rtype_index, size_t& _rid, const SuperFindFunctionT& _find_fnc) const;
+};
+
+template <class T, class S = SuperBase>
+struct Super : S {
+
+    using SuperT = S;
+
+    template <typename... Args>
+    explicit Super(Args&&... _args)
+        : S(std::forward<Args>(_args)...)
+    {
+    }
+
+    bool superFind(std::type_index& _rtype_index, size_t& _rid, const SuperFindFunctionT& _find_fnc) const override
+    {
+        {
+            const std::type_index ti(typeid(T));
+            if (_find_fnc(ti, _rid)) {
+                _rtype_index = ti;
+                return true;
+            }
+        }
+        return SuperT::superFind(_rtype_index, _rid, _find_fnc);
+    }
+};
+} //namespace solid
