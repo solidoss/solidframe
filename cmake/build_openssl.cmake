aaca5c6e8c3d857f76ff9fe24081dbf3e0541d99
Valentin Palade <vipalade@gmail.com>
2021-09-11 10:30:19 +0300

    work: improving openssl build

--- cmake/build_openssl.cmake
+++ cmake/build_openssl.cmake
@@ -13,7 +13,7 @@ if(SOLID_ON_WINDOWS)
         DOWNLOAD_NO_PROGRESS OFF
         #URL_MD5 "6f4571e7c5a66ccc3323da6c24be8f05"
         #CONFIGURE_COMMAND ${EXTERNAL_DIR}/openssl/src/build-openssl/config --prefix=${EXTERNAL_DIR} --openssldir=ssl_
-        CONFIGURE_COMMAND C:/Strawberry/perl/bin/perl Configure ${OPENSSL_TARGET} --prefix=${EXTERNAL_DIR} no-shared no-unit-test no-tests no-ui
+        CONFIGURE_COMMAND C:/Strawberry/perl/bin/perl Configure ${OPENSSL_TARGET} --prefix=${EXTERNAL_DIR} no-shared no-unit-test no-tests no-ui-console --libdir=lib
         BUILD_COMMAND nmake
         INSTALL_COMMAND nmake install_sw
         BUILD_IN_SOURCE 1
@@ -37,7 +37,7 @@ else()
         URL https://www.openssl.org/source/openssl-3.0.0.tar.gz
         DOWNLOAD_NO_PROGRESS ON
         #URL_MD5 "6f4571e7c5a66ccc3323da6c24be8f05"
-        CONFIGURE_COMMAND ${EXTERNAL_DIR}/openssl/src/build-openssl/config --prefix=${EXTERNAL_DIR} no-shared no-unit-test no-tests no-ui
+        CONFIGURE_COMMAND ${EXTERNAL_DIR}/openssl/src/build-openssl/config --prefix=${EXTERNAL_DIR} no-shared no-unit-test no-tests no-ui-console --libdir=lib
         BUILD_COMMAND make
         INSTALL_COMMAND make -j4 install_sw
         BUILD_IN_SOURCE 1
