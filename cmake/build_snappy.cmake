3b2096f46050a2d9fa8755070f7895621c9cfac4
Valentin Palade <vipalade@gmail.com>
2021-10-09 18:17:03 +0300

    change build external snappy

--- cmake/build_snappy.cmake
+++ cmake/build_snappy.cmake
@@ -4,12 +4,10 @@ ExternalProject_Add(
     build-snappy
     EXCLUDE_FROM_ALL 1
     PREFIX ${snappy_PREFIX}
-    URL https://github.com/google/snappy/archive/1.1.7.tar.gz
+    URL https://github.com/google/snappy/archive/1.1.9.tar.gz
     DOWNLOAD_NO_PROGRESS ON
     CMAKE_ARGS
-            -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/external -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_BUILD_TYPE=${CONFIGURATION_TYPE}
-    BUILD_COMMAND ${CMAKE_COMMAND} --build . --config ${CONFIGURATION_TYPE}
-    INSTALL_COMMAND ${CMAKE_COMMAND} --build . --config ${CONFIGURATION_TYPE} --target install
+            -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/external -DCMAKE_INSTALL_LIBDIR=lib -DSNAPPY_BUILD_TESTS=OFF -DSNAPPY_BUILD_BENCHMARKS=OFF
     LOG_UPDATE ON
     LOG_CONFIGURE ON
     LOG_BUILD ON
