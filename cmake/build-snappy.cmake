8277bc39d4aaa52908c08d884a0388ecef719860
Valentin Palade <vipalade@gmail.com>
2025-04-25 16:26:24 +0300

    update snappy

--- cmake/build-snappy.cmake
+++ cmake/build-snappy.cmake
@@ -10,7 +10,7 @@ ExternalProject_Add(
     build-snappy
     EXCLUDE_FROM_ALL 1
     PREFIX ${snappy_PREFIX}
-    URL https://github.com/google/snappy/archive/1.1.9.tar.gz
+    URL https://github.com/google/snappy/archive/refs/tags/1.2.2.tar.gz
     DOWNLOAD_NO_PROGRESS ON
     CMAKE_ARGS
             -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/external -DCMAKE_INSTALL_LIBDIR=lib -DSNAPPY_BUILD_TESTS=OFF -DSNAPPY_BUILD_BENCHMARKS=OFF
